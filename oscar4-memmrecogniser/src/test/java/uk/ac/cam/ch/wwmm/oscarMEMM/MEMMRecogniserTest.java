fe48f6008c79077ca7e8e63bc121137d903f792e
Egon Willighagen <egon.willighagen@gmail.com>
2010-10-06 07:49:54 +0200

    Added unit testing for constructors

---
+++ oscar4-memmrecogniser/src/test/java/uk/ac/cam/ch/wwmm/oscarMEMM/MEMMRecogniserTest.java
@@ -0,0 +1,15 @@
+package uk.ac.cam.ch.wwmm.oscarMEMM;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author egonw
+ */
+public class MEMMRecogniserTest {
+
+	@Test public void testConstructor() {
+		Assert.assertNotNull(new MEMMRecogniser());
+	}
+
+}

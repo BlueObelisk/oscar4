65568b53f26ce146d0c9872c4ccd01ea5196e8fe
Egon Willighagen <egonw@users.noreply.github.com>
2010-10-06 07:49:54 +0200

    Added unit testing for constructors

---
+++ oscar4-patternrecogniser/src/test/java/uk/ac/cam/ch/wwmm/oscarpattern/PatternRecogniserTest.java
@@ -0,0 +1,15 @@
+package uk.ac.cam.ch.wwmm.oscarpattern;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author egonw
+ */
+public class PatternRecogniserTest {
+
+	@Test public void testConstructor() {
+		Assert.assertNotNull(new PatternRecogniser());
+	}
+
+}

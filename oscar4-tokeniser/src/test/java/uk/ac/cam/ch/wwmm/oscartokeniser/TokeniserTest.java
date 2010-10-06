24334db582390c55dd1c53133b766721e47a27d1
Egon Willighagen <egonw@users.noreply.github.com>
2010-10-06 07:49:54 +0200

    Added unit testing for constructors

---
+++ oscar4-tokeniser/src/test/java/uk/ac/cam/ch/wwmm/oscartokeniser/TokeniserTest.java
@@ -0,0 +1,14 @@
+package uk.ac.cam.ch.wwmm.oscartokeniser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author egonw
+ */
+public final class TokeniserTest {
+
+	@Test public void testConstructor() {
+		Assert.assertNotNull(new Tokeniser());
+	}
+}

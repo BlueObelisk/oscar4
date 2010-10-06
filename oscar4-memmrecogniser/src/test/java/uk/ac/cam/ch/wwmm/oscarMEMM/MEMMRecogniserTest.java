c2a1d3bd696879982f21feafab95152fd566244b
Egon Willighagen <egon.willighagen@gmail.com>
2010-10-06 16:27:31 +0200

    A unit test

--- oscar4-memmrecogniser/src/test/java/uk/ac/cam/ch/wwmm/oscarMEMM/MEMMRecogniserTest.java
+++ oscar4-memmrecogniser/src/test/java/uk/ac/cam/ch/wwmm/oscarMEMM/MEMMRecogniserTest.java
@@ -1,10 +1,21 @@
 package uk.ac.cam.ch.wwmm.oscarMEMM;
 
+import java.util.List;
+
+import nu.xom.Document;
+
 import org.junit.Assert;
 import org.junit.Test;
 
+import uk.ac.cam.ch.wwmm.oscar.document.NamedEntity;
+import uk.ac.cam.ch.wwmm.oscar.document.ProcessingDocument;
+import uk.ac.cam.ch.wwmm.oscar.document.ProcessingDocumentFactory;
+import uk.ac.cam.ch.wwmm.oscar.tools.ResourceGetter;
+import uk.ac.cam.ch.wwmm.oscarMEMM.scixml.TextToSciXML;
+
 /**
  * @author egonw
+ * @author j_robinson
  */
 public class MEMMRecogniserTest {
 
@@ -12,4 +23,19 @@ public class MEMMRecogniserTest {
 		Assert.assertNotNull(new MEMMRecogniser());
 	}
 
+	@Test
+	public void testFindNamedEntities() throws Exception {
+		ResourceGetter rg = new ResourceGetter("uk/ac/cam/ch/wwmm/oscar3/test/testcard/resources/");
+		String s = rg.getString("testcard.txt");
+		Assert.assertTrue("Have testcard string", s != null && s.length() > 0);
+		Document doc = TextToSciXML.textToSciXML(s);
+		
+		ProcessingDocument procDoc = ProcessingDocumentFactory.getInstance().makeTokenisedDocument(doc, false, false, false);
+		Assert.assertTrue(procDoc != null);
+		List<NamedEntity> neList;
+		ChemicalEntityRecogniser cei = new MEMMRecogniser();
+		neList = cei.findNamedEntities(procDoc.getTokenSequences());
+		Assert.assertTrue(neList != null);
+		Assert.assertTrue(neList.size() > 0);
+	}
 }

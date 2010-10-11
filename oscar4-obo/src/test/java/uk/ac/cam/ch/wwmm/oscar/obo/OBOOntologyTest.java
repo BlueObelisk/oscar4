9dda35c8459d8e1cebe274ea6db81fb890cffc61
Egon Willighagen <egonw@users.noreply.github.com>
2010-10-11 15:17:07 +0100

    Factored out some OBO related classes

---
+++ oscar4-obo/src/test/java/uk/ac/cam/ch/wwmm/oscar/obo/OBOOntologyTest.java
@@ -0,0 +1,15 @@
+package uk.ac.cam.ch.wwmm.oscar.obo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OBOOntologyTest {
+
+	@Test
+	public void testGetInstance() {
+		OBOOntology obo = OBOOntology.getInstance();
+		Assert.assertNotNull(obo);
+		Assert.assertNotSame(0, obo.getTerms().size());
+	}
+
+}

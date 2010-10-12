7deddbc6cd04a49985df30ba682945625b71c7df
Egon Willighagen <egon.willighagen@gmail.com>
2010-10-12 14:38:22 +0100

    Replaced use of OscarProperty.verbose with log4j Logger

--- oscar4-memmrecogniser/src/main/java/uk/ac/cam/ch/wwmm/oscarMEMM/memm/document/HyphenTokeniser.java
+++ oscar4-memmrecogniser/src/main/java/uk/ac/cam/ch/wwmm/oscarMEMM/memm/document/HyphenTokeniser.java
@@ -5,6 +5,8 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.log4j.Logger;
+
 import uk.ac.cam.ch.wwmm.oscar.tools.OscarProperties;
 import uk.ac.cam.ch.wwmm.oscar.tools.StringTools;
 import uk.ac.cam.ch.wwmm.oscarMEMM.models.ExtractTrainingData;
@@ -24,7 +26,9 @@ import uk.ac.cam.ch.wwmm.oscarMEMM.terms.TermSets;
  * 4) Don't split!
  */
 public final class HyphenTokeniser {
-	
+
+	private final Logger logger = Logger.getLogger(HyphenTokeniser.class);
+
 	private static HyphenTokeniser myInstance = null;
 	private Set<String> splitSuffixes;
 	private Set<String> noSplitPrefixes;
@@ -53,7 +57,7 @@ public final class HyphenTokeniser {
 	}
 	
 	private HyphenTokeniser() throws Exception {
-		if(OscarProperties.getInstance().verbose) System.out.print("Initialising hyphen tokeniser... ");
+		logger.debug("Initialising hyphen tokeniser... ");
 		//splitSuffixes = TermSets.getSplitSuffixes();
 		splitSuffixes = new HashSet<String>();
 		splitSuffixes.addAll(ExtractTrainingData.getInstance().afterHyphen);
@@ -74,7 +78,7 @@ public final class HyphenTokeniser {
 		}
 
 		splitOnEnDash = OscarProperties.getInstance().splitOnEnDash;
-		if(OscarProperties.getInstance().verbose) System.out.println("hyphen tokeniser initialised");
+		logger.debug("hyphen tokeniser initialised");
 	}
 	
 	/** Initialises the singleton associated with this class. For convenience at startup.

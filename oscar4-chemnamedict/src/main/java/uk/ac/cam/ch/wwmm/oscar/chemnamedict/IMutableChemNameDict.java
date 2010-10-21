c328e1a6deb5efcfce1ede031dd2b0bec73cd24c
Egon Willighagen <egonw@users.noreply.github.com>
2010-10-21 16:32:34 +0200

    Factored out IO from the data classes

--- oscar4-chemnamedict/src/main/java/uk/ac/cam/ch/wwmm/oscar/chemnamedict/IMutableChemNameDict.java
+++ oscar4-chemnamedict/src/main/java/uk/ac/cam/ch/wwmm/oscar/chemnamedict/IMutableChemNameDict.java
@@ -2,12 +2,16 @@ package uk.ac.cam.ch.wwmm.oscar.chemnamedict;
 
 import java.util.Set;
 
+import uk.ac.cam.ch.wwmm.oscar.chemnamedict.data.ChemRecord;
+
 public interface IMutableChemNameDict extends IChemNameDict {
 
 	public void addStopWord(String word) throws Exception;
 
 	public void addChemRecord(String inchi, String smiles,
 			Set<String> names, Set<String> ontIDs) throws Exception;
+
+	public void addChemRecord(ChemRecord record) throws Exception;
 
 	public void addName(String name) throws Exception;
 

ac781743d5066833f94778413a130f2e1fc310b5
Egon Willighagen <egonw@users.noreply.github.com>
2010-10-28 09:11:15 +0200

    Added a method to load default dictionaries

--- oscar4-api/src/test/java/uk/ac/cam/ch/wwmm/oscar/OscarTest.java
+++ oscar4-api/src/test/java/uk/ac/cam/ch/wwmm/oscar/OscarTest.java
@@ -3,6 +3,8 @@ package uk.ac.cam.ch.wwmm.oscar;
 import org.junit.Assert;
 import org.junit.Test;
 
+import uk.ac.cam.ch.wwmm.oscar.chemnamedict.ChemNameDictRegistry;
+
 public class OscarTest {
 
 	@Test
@@ -17,4 +19,20 @@ public class OscarTest {
 		Assert.assertNotNull(oscar.getChemNameDict());
 	}
 	
+	@Test
+	public void testOscarLoadingOfDefaultDictionaries() throws Exception {
+		Oscar oscar = new Oscar(this.getClass().getClassLoader());
+		oscar.loadDefaultDictionaries();
+		ChemNameDictRegistry registry = oscar.getChemNameDict();
+		// test loading of the ChEBI dictionary
+		Assert.assertEquals(
+			"InChI=1/CH4/h1H4",
+			registry.getInChI("methane").iterator().next()
+		);
+		// test loading of the default dictionary
+		Assert.assertEquals(
+			"InChI=1/C4H6O3/c1-3(5)7-4(2)6/h1-2H3",
+			registry.getInChI("Ac2O").iterator().next()
+		);
+	}
 }

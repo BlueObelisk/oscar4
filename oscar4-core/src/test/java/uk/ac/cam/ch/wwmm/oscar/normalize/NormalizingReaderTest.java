7af8efc17eda26a0f1d9f5ef5b9bea232a4aac38
Egon Willighagen <egonw@users.noreply.github.com>
2010-11-04 18:42:22 +0100

    Added a framework for normalization

---
+++ oscar4-core/src/test/java/uk/ac/cam/ch/wwmm/oscar/normalize/NormalizingReaderTest.java
@@ -0,0 +1,24 @@
+package uk.ac.cam.ch.wwmm.oscar.normalize;
+
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class NormalizingReaderTest {
+
+	@Test
+	public void testUTF8() throws IOException {
+		NormalizingReader stream = new NormalizingReader( 
+			this.getClass().getClassLoader().getResourceAsStream(
+				"test.txt"
+			)
+		);
+		
+		String string = stream.readLine();
+		Assert.assertEquals(
+			"ff wachten", string
+		);
+	}
+}

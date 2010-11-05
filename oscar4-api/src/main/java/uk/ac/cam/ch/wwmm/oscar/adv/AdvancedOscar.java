3dc444587a83a58e99efcea218591c3f189d99a9
Egon Willighagen <egonw@users.noreply.github.com>
2010-11-05 09:43:53 +0100

    Removed unused imports

--- oscar4-api/src/main/java/uk/ac/cam/ch/wwmm/oscar/adv/AdvancedOscar.java
+++ oscar4-api/src/main/java/uk/ac/cam/ch/wwmm/oscar/adv/AdvancedOscar.java
@@ -1,11 +1,8 @@
 package uk.ac.cam.ch.wwmm.oscar.adv;
 
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 import uk.ac.cam.ch.wwmm.oscar.Oscar;
-import uk.ac.cam.ch.wwmm.oscar.document.ITokeniser;
-import uk.ac.cam.ch.wwmm.oscar.interfaces.ChemicalEntityRecogniser;
 
 public class AdvancedOscar extends Oscar {
 

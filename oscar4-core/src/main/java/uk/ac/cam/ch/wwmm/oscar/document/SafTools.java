337b674b67f48319ca5d5d96919b41ed20e1ed03
dmj30 <dmj30@localhost>
2010-11-11 14:53:02 +0000

    Removed duplicated class. Retained version from PatternRecogniser project, which had been refactored to improve speed of execution.

--- oscar4-core/src/main/java/uk/ac/cam/ch/wwmm/oscar/document/SafTools.java
+++ oscar4-core/src/main/java/uk/ac/cam/ch/wwmm/oscar/document/SafTools.java
@@ -3,8 +3,8 @@ package uk.ac.cam.ch.wwmm.oscar.document;
 import nu.xom.Attribute;
 import nu.xom.Document;
 import nu.xom.Element;
+import nu.xom.Elements;
 import nu.xom.Nodes;
-
 
 /** Generic routines for handling standoff annotations in SAF format.
  * 
@@ -20,8 +20,9 @@ public final class SafTools {
 	 * @param slotVal The slot value.
 	 */
 	public static void setSlot(Element annot, String slotName, String slotVal) {
-		Nodes n = annot.query("slot[@name=\"" + slotName + "\"]");
-		for(int i=0;i<n.size();i++) n.get(i).detach();
+		//        Nodes n = annot.query("slot[@name=\"" + slotName + "\"]");
+        //		for(int i=0;i<n.size();i++) n.get(i).detach();
+        removeSlot(annot, slotName);
 
 		Element slot = new Element("slot");
 		slot.addAttribute(new Attribute("name", slotName));
@@ -36,12 +37,24 @@ public final class SafTools {
 	 * @return The slot value, or null.
 	 */
 	public static String getSlotValue(Element annot, String slotName) {
-		Nodes n = annot.query("slot[@name=\"" + slotName + "\"]");
-		if(n.size() > 0) {
-			return n.get(0).getValue();
-		} else {
-			return null;
-		}
+		Elements els = annot.getChildElements("slot");
+        for (int i = 0; i < els.size(); i++) {
+            Element e = els.get(i);
+            String name = e.getAttributeValue("name");
+            if (slotName.equals(name)) {
+                return e.getValue();
+            }
+        }
+        return null;
+
+//        Nodes n = annot.query("slot[@name=\"" + slotName + "\"]");
+//
+//
+//		if(n.size() > 0) {
+//			return n.get(0).getValue();
+//		} else {
+//			return null;
+//		}
 	}
 	
 	/**Deletes a slot (if it exists) from an annot.
@@ -50,14 +63,19 @@ public final class SafTools {
 	 * @param slotName The slot name.
 	 */
 	public static void removeSlot(Element annot, String slotName) {
-		Nodes n = annot.query("slot[@name=\"" + slotName + "\"]");
-		if(n.size() > 0) {
-			for(int i=0;i<n.size();i++) {
-				n.get(i).detach();
-			}
-		} else {
-			return;
-		}
+//		Nodes n = annot.query("slot[@name=\"" + slotName + "\"]");
+//		if(n.size() > 0) {
+//			for(int i=0;i<n.size();i++) {
+//				n.get(i).detach();
+//			}
+//		}
+        Elements els = annot.getChildElements("slot");
+        for (int i = els.size()-1; i >= 0; i--) {
+            Element e = els.get(i);
+            if (slotName.equals(e.getAttributeValue("name"))) {
+                e.detach();
+            }
+        }
 	}
 
 	/**Makes an empty annot element.

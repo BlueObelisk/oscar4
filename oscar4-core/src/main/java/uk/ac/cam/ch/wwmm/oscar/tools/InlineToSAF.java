6fab71486e1a79c4e8451ba8563d1c8c99450290
Sam Adams <seadams@gmail.com>
2010-11-17 15:53:33 +0000

    Restored ExtractedTrainingDataBuilder

--- oscar4-core/src/main/java/uk/ac/cam/ch/wwmm/oscar/tools/InlineToSAF.java
+++ oscar4-core/src/main/java/uk/ac/cam/ch/wwmm/oscar/tools/InlineToSAF.java
@@ -1,11 +1,10 @@
-package uk.ac.cam.ch.wwmm.oscarMEMM.models;
+package uk.ac.cam.ch.wwmm.oscar.tools;
 
 import nu.xom.Attribute;
 import nu.xom.Document;
 import nu.xom.Element;
 import nu.xom.Nodes;
 import uk.ac.cam.ch.wwmm.oscar.document.SafTools;
-import uk.ac.cam.ch.wwmm.oscar.tools.StandoffTable;
 import uk.ac.cam.ch.wwmm.oscar.xmltools.XMLSpanTagger;
 
 /** Converts inline annotation to standoff annotation.

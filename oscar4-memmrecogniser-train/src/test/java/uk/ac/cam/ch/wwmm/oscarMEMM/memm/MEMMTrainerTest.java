c5255783360c6d30849784475fc3fdd07e35e5b2
Egon Willighagen <egonw@users.noreply.github.com>
2010-11-17 11:37:10 +0100

    Added initial unit test for the training code

--- oscar4-memmrecogniser-train/src/test/java/uk/ac/cam/ch/wwmm/oscarMEMM/memm/MEMMTrainerTest.java
+++ oscar4-memmrecogniser-train/src/test/java/uk/ac/cam/ch/wwmm/oscarMEMM/memm/MEMMTrainerTest.java
@@ -1,5 +1,7 @@
 package uk.ac.cam.ch.wwmm.oscarMEMM.memm;
 
+import java.io.InputStream;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,5 +11,16 @@ public class MEMMTrainerTest {
 	public void testConstructor() throws Exception {
 		MEMMTrainer trainer = new MEMMTrainer();
 		Assert.assertNotNull(trainer);
+	}
+
+	@Test
+	public void testLearning() throws Exception {
+		MEMMTrainer trainer = new MEMMTrainer();
+		InputStream stream = this.getClass().getClassLoader().getResourceAsStream(
+			"uk/ac/cam/ch/wwmm/oscarMEMM/memm/paper.xml"
+		);
+		Assert.assertNotNull(stream);
+		trainer.trainOnStream(stream, "what to enter here??");
+		// FIXME: how to test that it learned something??
 	}
 }

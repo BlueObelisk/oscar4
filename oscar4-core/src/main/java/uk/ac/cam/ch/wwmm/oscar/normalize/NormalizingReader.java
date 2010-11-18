ca039cbdf7b1e9c4916427f5c9282a988a0d7d1d
Sam Adams <seadams@gmail.com>
2010-11-18 12:19:52 +0000

    Removed unused code

--- oscar4-core/src/main/java/uk/ac/cam/ch/wwmm/oscar/normalize/NormalizingReader.java
+++ oscar4-core/src/main/java/uk/ac/cam/ch/wwmm/oscar/normalize/NormalizingReader.java
@@ -10,13 +10,11 @@ public class NormalizingReader {
 	private ITextNormalizer normalizer;
 
 	private BufferedReader source;
-	String currentReplacement;
 
 	public NormalizingReader(InputStream stream) {
 		this.source = new BufferedReader(
 			new InputStreamReader(stream)
 		);
-		currentReplacement = null;
 		normalizer = Normalizer.getDefaultInstance();
 	}
 

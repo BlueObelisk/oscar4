b097ff252790990f3a3f4621a4666e9b948ba12d
dmj30 <dmj30@localhost>
2010-11-18 17:31:56 +0000

    Refactoring of and unit tests for HyphenTokeniser.

--- oscar4-tokeniser/src/main/java/uk/ac/cam/ch/wwmm/oscartokeniser/HyphenTokeniser.java
+++ oscar4-tokeniser/src/main/java/uk/ac/cam/ch/wwmm/oscartokeniser/HyphenTokeniser.java
@@ -31,9 +31,11 @@ public final class HyphenTokeniser {
 	private Set<String> noSplitPrefixes;
 	private int minPrefixLength;
 	private int maxPrefixLength;
+	//splitOnEnDash will become redundant with the normalisation of hyphens
 	private boolean splitOnEnDash;
 	private static Pattern suffixPrefixPattern = Pattern.compile("mono|di|tri|tetra|penta|hexa|hepta|un|de|re|pre");
 	private static Pattern propernounHyphenPattern = Pattern.compile("((Mc|Mac)?[A-Z]\\p{Ll}\\p{Ll}\\p{Ll}+(s'|'s)?" + StringTools.hyphensRegex + ")+(Mc|Mac)?[A-Z]\\p{Ll}\\p{Ll}\\p{Ll}+(s'|'s)?");
+	private static Pattern lowercaseEitherSidePattern = Pattern.compile("[a-z][a-z]["+StringTools.hyphens+"][a-z]+");
 	
 	/**Re-initialises the Hyphen tokeniser.
 	 * 
@@ -44,7 +46,7 @@ public final class HyphenTokeniser {
 		getInstance();
 	}
 	
-	private static HyphenTokeniser getInstance() {
+	static HyphenTokeniser getInstance() {
 		try {
 			if(myInstance == null) myInstance = new HyphenTokeniser();
 		} catch (Exception e) {
@@ -56,16 +58,8 @@ public final class HyphenTokeniser {
 	private HyphenTokeniser() throws Exception {
 		Logger logger = Logger.getLogger(HyphenTokeniser.class);
 		logger.debug("Initialising hyphen tokeniser... ");
-		//splitSuffixes = TermSets.getSplitSuffixes();
 		splitSuffixes = new HashSet<String>();
 		splitSuffixes.addAll(TermSets.getDefaultInstance().getSplitSuffixes());
-		
-		/*for(String s : TermSets.getUsrDictWords()) {
-			if(!ChemNameDictSingleton.hasName(s) && !ExtractTrainingData.getIntstance().chemicalWords.contains(s)) {
-				splitSuffixes.add(s);
-			}
-		}*/
-		
 		noSplitPrefixes = TermSets.getDefaultInstance().getNoSplitPrefixes();
 		
 		minPrefixLength = 1000;
@@ -75,24 +69,20 @@ public final class HyphenTokeniser {
 			maxPrefixLength = Math.max(maxPrefixLength, p.length());
 		}
 
+
 		splitOnEnDash = OscarProperties.getData().splitOnEnDash;
 		logger.debug("hyphen tokeniser initialised");
 	}
 	
-	/** Initialises the singleton associated with this class. For convenience at startup.
-	 */
-	public static void init() {
-		getInstance();
-	}
 	
 	/**Works out where, if anywhere, to split a string which may contain a hyphen
 	 * 
-	 * @param s The string to analyse
+	 * @param tokenValue The string to analyse
 	 * @return The index of the hyphen to split at, or -1
 	 */
-	public static int indexOfSplittableHyphen(String s) {
+	public static int indexOfSplittableHyphen(String tokenValue) {
 		try {
-			return getInstance().indexOfSplittableHyphenInternal(s);
+			return getInstance().indexOfSplittableHyphenInternal(tokenValue);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return -1;
@@ -102,74 +92,157 @@ public final class HyphenTokeniser {
 	private int indexOfSplittableHyphenInternal(String tokenValue) {
 		//TODO break each tokenisation case into a separate method and unit test
 		boolean balancedBrackets = StringTools.bracketsAreBalanced(tokenValue);
-		for(int currentIndex=tokenValue.length()-3;currentIndex>0;currentIndex--) {
+		
+		//read from the back of the string, keeping at least two characters after the hyphen
+		for(int currentIndex=tokenValue.length()-3; currentIndex > 0; currentIndex--) {
 			/* Are we looking at a hyphen? */
 			if(StringTools.hyphens.indexOf(tokenValue.codePointAt(currentIndex)) != -1) {
-				
-				//Don't split on tokens contained within brackets
-				if(balancedBrackets && !StringTools.bracketsAreBalanced(tokenValue.substring(currentIndex+1))) continue;
-				
-				// Split on en-dashes?
-				if(splitOnEnDash && tokenValue.substring(currentIndex, currentIndex+1).equals(StringTools.enDash)) return currentIndex;
-				
-				// Always split on em-dashes
-				if(tokenValue.substring(currentIndex, currentIndex+1).equals(StringTools.emDash)) return currentIndex;				
-				
-				if(OntologyTerms.getHyphTokable().contains(StringTools.normaliseName(tokenValue.substring(0,currentIndex)) + " " + 
-						StringTools.normaliseName(tokenValue.substring(currentIndex+1)))) {
-					return currentIndex;
-				}
-				// Suffixes?
-				String suffix = tokenValue.substring(currentIndex+1).toLowerCase();
-				if(splitSuffixes.contains(suffix)) {
-					return currentIndex;
+				//Don't split on hyphens contained within brackets
+				if(balancedBrackets && !StringTools.bracketsAreBalanced(tokenValue.substring(currentIndex+1))) {
+					continue;
 				}
 				
-				Matcher m = propernounHyphenPattern.matcher(tokenValue);
-				if(m.matches()) {
-					return currentIndex;
-				}
+				//the en-dash and em-dash rules will become redundant with string normalisation
 				
-				m = suffixPrefixPattern.matcher(suffix); 
-				if(m.lookingAt()) {
-					//the suffix string is what remains of the string when certain
-					//prefixes (defined in suffixPrefixPattern) are removed
-					suffix = suffix.substring(m.end());
-				}
-				while(suffix.length() >= 3) {
-					//we check to see if any of the splitSuffixes follow an
-					//identified prefix
-					if(splitSuffixes.contains(suffix)) {
+				//split on en-dash
+				if (splitOnEnDash) {
+					if(checkEnDash(tokenValue, currentIndex)) { 
 						return currentIndex;
 					}
-					suffix = suffix.substring(0, suffix.length()-1);
+				}
+				
+				// Always split on em-dashes
+				if (checkEmDash(tokenValue, currentIndex)) {
+					return currentIndex;				
+				}
+				
+				// Check against OntologyTerms content
+				if (termContainedInHyphTokable(tokenValue, currentIndex)) {
+					return currentIndex;
+				}
+				
+				// Suffixes?
+				if (suffixContainedInSplitSuffix(tokenValue, currentIndex)) {
+					return currentIndex;
+				}
+				
+				if (termMatchesPropernounPattern(tokenValue)) {
+					return currentIndex;
+				}
+				
+				if (suffixStartsWithSplitSuffix(tokenValue, currentIndex)) {
+					return currentIndex;
 				}
 				
 				// No suffix? Then don't examine hyphens in position 1
-				if(currentIndex == 1) continue;
+				if(currentIndex == 1) {
+					continue;
+				}
 				
 				// Prefixes
-				// check to see if the word contains one of the noSplitPrefixes. If
-				// it does, don't tokenise.
-				boolean noSplit = false;
-				for(int j=minPrefixLength;j<=maxPrefixLength && j<=currentIndex;j++) {
-					String prefix = tokenValue.substring(currentIndex-j, currentIndex).toLowerCase();
-					if(noSplitPrefixes.contains(prefix)) {
-						noSplit = true;
-						break;
-					}
-				}
-				if(noSplit) {
+				if (precededByNoSplitPrefix(tokenValue, currentIndex)) {
 					continue;
 				}
 				
 				/* Check for lowercase either side of the token */
-				if(tokenValue.substring(currentIndex-2).matches("[a-z][a-z]["+StringTools.hyphens+"][a-z]+")) {
+				if (lowercaseEitherSideOfHyphen(tokenValue, currentIndex)) {
 					return currentIndex;
 				}
+				
 			}
 		}
 		return -1;
+	}
+
+
+	/**
+	 * checks if the substring of the term starting two characters before the hyphen
+	 * matches the lowercaseEitherSidePattern, i.e.
+	 * 
+	 *  lowercase lowercase hyphen (lowercase)+ 
+	 */
+	boolean lowercaseEitherSideOfHyphen(String tokenValue, int currentIndex) {
+		Matcher matcher = lowercaseEitherSidePattern.matcher(tokenValue.substring(currentIndex-2));
+		return matcher.matches();
+	}
+
+	/**
+	 * checks if one of the terms from the noSplitPrefix list occurs before the hyphen 
+	 */
+	private boolean precededByNoSplitPrefix(String tokenValue, int currentIndex) {
+		for(int j = minPrefixLength; j <= maxPrefixLength && j <= currentIndex; j++) {
+			String prefix = tokenValue.substring(currentIndex-j, currentIndex).toLowerCase();
+			if(noSplitPrefixes.contains(prefix)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * checks if one of the terms from the splitSuffix list occurs after the hyphen 
+	 */
+	boolean suffixStartsWithSplitSuffix(String tokenValue, int currentIndex) {
+		String suffix = tokenValue.substring(currentIndex+1).toLowerCase();
+		Matcher m = suffixPrefixPattern.matcher(suffix); 
+		//strip certain strings from the beginning of the suffix
+		if(m.lookingAt()) {
+			suffix = suffix.substring(m.end());
+		}
+
+		while(suffix.length() >= 3) {
+			if(splitSuffixes.contains(suffix)) {
+				return true;
+			}
+			suffix = suffix.substring(0, suffix.length()-1);
+		}
+		
+		return false;
+	}
+
+	/**
+	 * Checks if the term matches the properNounHyphenPattern  
+	 */
+	boolean termMatchesPropernounPattern(String tokenValue) {
+		Matcher m = propernounHyphenPattern.matcher(tokenValue);
+		return m.matches();
+	}
+
+	/**
+	 * Checks if the portion of the string occurring after the hyphen is
+	 * contained in the split suffixes list. 
+	 */
+	boolean suffixContainedInSplitSuffix(String tokenValue, int currentIndex) {
+		String suffix = tokenValue.substring(currentIndex+1).toLowerCase();
+		return splitSuffixes.contains(suffix);
+	}
+
+	/**
+	 * Checks if the OntologyTerms contains an equivalent term to the tokenValue
+	 * in which the hyphen at the currentIndex has been replaced by a space
+	 */
+	private boolean termContainedInHyphTokable(String tokenValue, int currentIndex) {
+		StringBuilder builder = new StringBuilder(tokenValue.length());
+		builder.append(StringTools.normaliseName(tokenValue.substring(0,currentIndex)));
+		builder.append(" ");
+		builder.append(StringTools.normaliseName(tokenValue.substring(currentIndex+1)));
+		
+		return OntologyTerms.getHyphTokable().contains(builder.toString());
+	}
+
+	/**
+	 * Checks if the character at the currentIndex is em-dash
+	 */
+	private boolean checkEmDash(String tokenValue, int currentIndex) {
+		return tokenValue.substring(currentIndex, currentIndex+1).equals(StringTools.emDash);
+	}
+
+	/**
+	 * Checks if the character at the currentIndex is en-dash 
+	 * 
+	 */
+	private boolean checkEnDash(String tokenValue, int currentIndex) {
+		return tokenValue.substring(currentIndex, currentIndex+1).equals(StringTools.enDash);
 	}
 	
 }

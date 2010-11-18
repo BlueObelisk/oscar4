2adefb92e0731c392e6aea60a8084a2cc4f8e1b3
Egon Willighagen <egonw@users.noreply.github.com>
2010-11-18 17:09:11 +0100

    More detailed testing of the training output

--- oscar4-memmrecogniser-train/src/test/java/uk/ac/cam/ch/wwmm/oscarMEMM/memm/MEMMTrainerTest.java
+++ oscar4-memmrecogniser-train/src/test/java/uk/ac/cam/ch/wwmm/oscarMEMM/memm/MEMMTrainerTest.java
@@ -2,35 +2,57 @@ package uk.ac.cam.ch.wwmm.oscarMEMM.memm;
 
 import java.io.InputStream;
 
+import nu.xom.Document;
+import nu.xom.Element;
+import nu.xom.Elements;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import uk.ac.cam.ch.wwmm.oscarMEMM.models.Model;
+
+import ch.unibe.jexample.Given;
+import ch.unibe.jexample.Injection;
+import ch.unibe.jexample.InjectionPolicy;
+import ch.unibe.jexample.JExample;
+
+@RunWith(JExample.class)
 public class MEMMTrainerTest {
 
 	@Test
-	public void testConstructor() throws Exception {
+	public MEMMTrainer testConstructor() throws Exception {
 		MEMMTrainer trainer = new MEMMTrainer();
 		Assert.assertNotNull(trainer);
+		return trainer;
 	}
 
-	@Test
-	public void testUntrainedStatus() throws Exception {
-		MEMMTrainer trainer = new MEMMTrainer();
+	@Given("testConstructor")
+	public String testUntrainedStatus(MEMMTrainer trainer) throws Exception {
 		String xml = trainer.writeModel().toXML();
 		Assert.assertEquals("<memm />", xml);
+		return xml;
 	}
 
-	@Test
-	public void testLearning() throws Exception {
-		MEMMTrainer trainer = new MEMMTrainer();
+	@Given("testConstructor,testUntrainedStatus")
+	public void testLearning(MEMMTrainer trainer, String untrainedXML) throws Exception {
 		InputStream stream = this.getClass().getClassLoader().getResourceAsStream(
 			"uk/ac/cam/ch/wwmm/oscarMEMM/memm/paper.xml"
 		);
 		Assert.assertNotNull(stream);
-		String xml = trainer.writeModel().toXML();
 		trainer.trainOnStream(stream);
 		trainer.finishTraining();
-		Assert.assertNotSame(xml, trainer.writeModel().toXML());
-		// it must have learned something
+		Element trainedModel = trainer.writeModel();
+		Assert.assertNotSame(untrainedXML, trainedModel.toXML());
+
+		Assert.assertEquals("memm", trainedModel.getLocalName());
+		Elements elements = trainedModel.getChildElements();
+		for (int i=0; i<elements.size(); i++)
+			Assert.assertEquals("maxent", elements.get(i).getLocalName());
+
+		Element fullModel = Model.makeModel().getRootElement();
+		Assert.assertEquals("model", fullModel.getLocalName());
+		Assert.assertEquals(1, fullModel.getChildElements("etd").size());
+		Assert.assertEquals(1, fullModel.getChildElements("memm").size());
 	}
 }

c9cab6b7557c2ae69224dad01421d3b778953b21
Sam Adams <seadams@gmail.com>
2010-11-18 12:45:34 +0000

    Made Token fields private

--- oscar4-tokeniser/src/main/java/uk/ac/cam/ch/wwmm/oscartokeniser/Main.java
+++ oscar4-tokeniser/src/main/java/uk/ac/cam/ch/wwmm/oscartokeniser/Main.java
@@ -35,7 +35,7 @@ public class Main {
 			
 			System.out.println("There are "+tokens.size()+" tokens in the string");
 			for (Token token : tokens) {
-				System.out.println("Token:: "+token.value);
+				System.out.println("Token:: "+token.getValue());
 			}
 			
 		}

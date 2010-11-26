b70f87229e37d31d31139a69e0b2000147c70b34
Egon Willighagen <egon.willighagen@gmail.com>
2010-11-26 06:48:52 +0100

    Added unit test to see if it HPEI works via the registry too

--- oscar4-chemnamedict/src/test/java/uk/ac/cam/ch/wwmm/oscar/chemnamedict/core/PolymerDictionaryTest.java
+++ oscar4-chemnamedict/src/test/java/uk/ac/cam/ch/wwmm/oscar/chemnamedict/core/PolymerDictionaryTest.java
@@ -3,6 +3,7 @@ package uk.ac.cam.ch.wwmm.oscar.chemnamedict.core;
 import org.junit.Assert;
 import org.junit.Test;
 
+import uk.ac.cam.ch.wwmm.oscar.chemnamedict.ChemNameDictRegistry;
 import uk.ac.cam.ch.wwmm.oscar.chemnamedict.IChemNameDict;
 
 public class PolymerDictionaryTest {
@@ -12,6 +13,15 @@ public class PolymerDictionaryTest {
 		IChemNameDict dict = new PolymerDictionary();
 		Assert.assertNotNull(dict);
 		Assert.assertTrue(dict.hasName("HPEI25k"));
+	}
+	
+	@Test
+	public void testAcccessViaRegistry() throws Exception {
+		IChemNameDict dict = new PolymerDictionary();
+		ChemNameDictRegistry registry = ChemNameDictRegistry.getInstance();
+		registry.register(dict);
+		Assert.assertNotNull(registry);
+		Assert.assertTrue(registry.hasName("HPEI25k"));
 	}
 	
 }

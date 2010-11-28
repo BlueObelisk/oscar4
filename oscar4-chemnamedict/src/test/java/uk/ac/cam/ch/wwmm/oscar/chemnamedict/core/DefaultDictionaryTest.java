fd6d6447c1eee5df9d41efd4878e54962c97a867
Egon Willighagen <egonw@users.noreply.github.com>
2010-11-28 18:28:23 +0100

    Also read the chemnamedict.xml

--- oscar4-chemnamedict/src/test/java/uk/ac/cam/ch/wwmm/oscar/chemnamedict/core/DefaultDictionaryTest.java
+++ oscar4-chemnamedict/src/test/java/uk/ac/cam/ch/wwmm/oscar/chemnamedict/core/DefaultDictionaryTest.java
@@ -11,10 +11,21 @@ public class DefaultDictionaryTest {
 	public void testACompound() throws Exception {
 		IInChIProvider dict = new DefaultDictionary();
 		Assert.assertNotNull(dict);
+		// from defaultCompounds.xml
 		Assert.assertEquals(
 			"InChI=1/C4H6O3/c1-3(5)7-4(2)6/h1-2H3",
 			dict.getInChI("Ac2O").iterator().next()
 		);
 	}
-	
+
+	@Test
+	public void testCompoundFromSecondFile() throws Exception {
+		// from chemnamedict.xml
+		IInChIProvider dict = new DefaultDictionary();
+		Assert.assertNotNull(dict);
+		Assert.assertEquals(
+			"InChI=1/H2O4S/c1-5(2,3)4/h(H2,1,2,3,4)/f/h1-2H",
+			dict.getInChI("sulfuric acid").iterator().next()
+		);
+	}
 }

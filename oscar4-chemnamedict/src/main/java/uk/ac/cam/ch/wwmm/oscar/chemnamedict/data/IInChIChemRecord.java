1f4070cdb069a06368656e86adc1815d66f08544
Egon Willighagen <egonw@users.noreply.github.com>
2010-11-29 15:21:13 +0100

    Extracted interfaces, allowing differentiation of ChemRecord (some dictionaries have no ontology identifiers, others no SMILES, etc)

---
+++ oscar4-chemnamedict/src/main/java/uk/ac/cam/ch/wwmm/oscar/chemnamedict/data/IInChIChemRecord.java
@@ -0,0 +1,24 @@
+package uk.ac.cam.ch.wwmm.oscar.chemnamedict.data;
+
+/**
+ * A {@link IChemRecord} that has support for an InChI identifier.
+ *
+ * @author egonw
+ */
+public interface IInChIChemRecord extends IChemRecord {
+
+	/**
+	 * Sets the InChI for this chemical compound.
+	 *
+	 * @param inchi
+	 */
+	public abstract void setInChI(String inchi);
+
+	/**
+	 * Returns the InChI for this compound.
+	 *
+	 * @return A {@link String} representation of the InChI.
+	 */
+	public abstract String getInChI();
+
+}
---
+++ oscar4-chemnamedict/src/main/java/uk/ac/cam/ch/wwmm/oscar/chemnamedict/data/ISMILESChemRecord.java
@@ -0,0 +1,24 @@
+package uk.ac.cam.ch.wwmm.oscar.chemnamedict.data;
+
+/**
+ * A {@link IChemRecord} that has support for a SMILES representation.
+ *
+ * @author egonw
+ */
+public interface ISMILESChemRecord extends IChemRecord {
+
+	/**
+	 * Sets the SMILES for this compound.
+	 *
+	 * @param smiles a {@link String} representation of the SMILES.
+	 */
+	public abstract void setSMILES(String smiles);
+
+	/**
+	 * Returns the SMILES for this compound.
+	 *
+	 * @return a {@link String} representation of the SMILES.
+	 */
+	public abstract String getSMILES();
+
+}

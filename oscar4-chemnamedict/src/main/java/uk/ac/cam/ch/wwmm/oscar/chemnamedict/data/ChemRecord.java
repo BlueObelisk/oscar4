42f0744da5a8f731c0216d38193775538c2b31c7
Egon Willighagen <egonw@users.noreply.github.com>
2010-11-29 15:21:13 +0100

    Extracted interfaces, allowing differentiation of ChemRecord (some dictionaries have no ontology identifiers, others no SMILES, etc)

--- oscar4-chemnamedict/src/main/java/uk/ac/cam/ch/wwmm/oscar/chemnamedict/data/ChemRecord.java
+++ oscar4-chemnamedict/src/main/java/uk/ac/cam/ch/wwmm/oscar/chemnamedict/data/ChemRecord.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-public class ChemRecord {
+public class ChemRecord implements IInChIChemRecord, ISMILESChemRecord, IOntologyChemRecord, IChemRecord {
 
 	private String inchi;
 	private String smiles;
@@ -19,42 +19,72 @@ public class ChemRecord {
 		ontIDs = new HashSet<String>();
 	}
 
+	/* (non-Javadoc)
+	 * @see uk.ac.cam.ch.wwmm.oscar.chemnamedict.data.IChemRecord#addName(java.lang.String)
+	 */
 	public void addName(String name) {
 		names.add(name);
 	}
 
+	/* (non-Javadoc)
+	 * @see uk.ac.cam.ch.wwmm.oscar.chemnamedict.data.IChemRecord#addNames(java.util.Collection)
+	 */
 	public void addNames(Collection<String> names) {
 		names.addAll(names);
 	}
 
+	/* (non-Javadoc)
+	 * @see uk.ac.cam.ch.wwmm.oscar.chemnamedict.data.IChemRecord#getNames()
+	 */
 	public Collection<String> getNames() {
 		return Collections.unmodifiableCollection(names);
 	}
 	
+	/* (non-Javadoc)
+	 * @see uk.ac.cam.ch.wwmm.oscar.chemnamedict.data.IOntologyChemRecord#addOntologyIdentifiers(java.util.Collection)
+	 */
 	public void addOntologyIdentifiers(Collection<String> identifiers) {
 		ontIDs.addAll(identifiers);
 	}
 
+	/* (non-Javadoc)
+	 * @see uk.ac.cam.ch.wwmm.oscar.chemnamedict.data.IOntologyChemRecord#addOntologyIdentifier(java.lang.String)
+	 */
 	public void addOntologyIdentifier(String identifier) {
 		ontIDs.add(identifier);
 	}
 
+	/* (non-Javadoc)
+	 * @see uk.ac.cam.ch.wwmm.oscar.chemnamedict.data.IOntologyChemRecord#getOntologyIdentifiers()
+	 */
 	public Collection<String> getOntologyIdentifiers() {
 		return Collections.unmodifiableCollection(ontIDs);
 	}
 
+	/* (non-Javadoc)
+	 * @see uk.ac.cam.ch.wwmm.oscar.chemnamedict.data.IInChIChemRecord#setInChI(java.lang.String)
+	 */
 	public void setInChI(String inchi) {
 		this.inchi = inchi;
 	}
 
+	/* (non-Javadoc)
+	 * @see uk.ac.cam.ch.wwmm.oscar.chemnamedict.data.IInChIChemRecord#getInChI()
+	 */
 	public String getInChI() {
 		return inchi;
 	}
 
+	/* (non-Javadoc)
+	 * @see uk.ac.cam.ch.wwmm.oscar.chemnamedict.data.ISMILESChemRecord#setSMILES(java.lang.String)
+	 */
 	public void setSMILES(String smiles) {
 		this.smiles = smiles;
 	}
 
+	/* (non-Javadoc)
+	 * @see uk.ac.cam.ch.wwmm.oscar.chemnamedict.data.ISMILESChemRecord#getSMILES()
+	 */
 	public String getSMILES() {
 		return smiles;
 	}
---
+++ oscar4-chemnamedict/src/main/java/uk/ac/cam/ch/wwmm/oscar/chemnamedict/data/IChemRecord.java
@@ -0,0 +1,33 @@
+package uk.ac.cam.ch.wwmm.oscar.chemnamedict.data;
+
+import java.util.Collection;
+
+/**
+ * A record reflecting linguistic information around a single chemical.
+ *
+ * @author egonw
+ */
+public interface IChemRecord {
+
+	/**
+	 * Adds a name to the record.
+	 *
+	 * @param name a name for the chemical.
+	 */
+	public abstract void addName(String name);
+
+	/**
+	 * Adds multiple names to the record.
+	 *
+	 * @param names the names for the chemical.
+	 */
+	public abstract void addNames(Collection<String> names);
+
+	/**
+	 * Returns a collection of names for this compound.
+	 *
+	 * @return a {@link Collection} with the names.
+	 */
+	public abstract Collection<String> getNames();
+
+}

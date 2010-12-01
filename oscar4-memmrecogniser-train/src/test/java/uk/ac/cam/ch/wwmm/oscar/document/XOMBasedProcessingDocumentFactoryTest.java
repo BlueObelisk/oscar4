480558bac5c84d07fcb469d509ef552395a7bc48
dmj30 <dmj30@cam.ac.uk>
2010-12-01 17:56:56 +0000

    Moved learning-specific tokenisation code to memmrecogniser-train project

--- oscar4-memmrecogniser-train/src/test/java/uk/ac/cam/ch/wwmm/oscar/document/XOMBasedProcessingDocumentFactoryTest.java
+++ oscar4-memmrecogniser-train/src/test/java/uk/ac/cam/ch/wwmm/oscar/document/XOMBasedProcessingDocumentFactoryTest.java
@@ -3,6 +3,8 @@ package uk.ac.cam.ch.wwmm.oscar.document;
 import static org.junit.Assert.*;
 
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
 
 import nu.xom.Builder;
 import nu.xom.Document;
@@ -15,15 +17,113 @@ import uk.ac.cam.ch.wwmm.oscartokeniser.Tokeniser;
 public class XOMBasedProcessingDocumentFactoryTest {
 
 	@Test
+	public void testTokeniseOnAnnotationBoundaries() throws Exception {
+		InputStream in = ClassLoader.getSystemResourceAsStream("uk/ac/cam/ch/wwmm/oscar/document/sciXmlPaper.xml");
+		Document sourceDoc = new Builder().build(in);
+//		"the quick methylbrown fox jumps over thechlorinated dog"
+		Tokeniser tokeniser = Tokeniser.getInstance();
+		XOMBasedProcessingDocument procDoc = (XOMBasedProcessingDocument) XOMBasedProcessingDocumentFactory.getInstance().makeTokenisedDocument(tokeniser, sourceDoc, false, false, false);
+		//empty tokenSequence for the empty HEADER in the sourceDoc
+		assertEquals(2, procDoc.getTokenSequences().size());
+		List <IToken> tokens = procDoc.getTokenSequences().get(1).getTokens(); 
+		assertEquals(8, tokens.size());
+		assertEquals("the", tokens.get(0).getValue());
+		assertEquals("quick", tokens.get(1).getValue());
+		assertEquals("methylbrown", tokens.get(2).getValue());
+		assertEquals("fox", tokens.get(3).getValue());
+		assertEquals("jumps", tokens.get(4).getValue());
+		assertEquals("over", tokens.get(5).getValue());
+		assertEquals("thechlorinated", tokens.get(6).getValue());
+		assertEquals("dog", tokens.get(7).getValue());
+		
+		String sourceString = "the quick methylbrown fox jumps over thechlorinated dog";
+		XOMBasedProcessingDocumentFactory.getInstance().tokeniseOnAnnotationBoundaries(tokeniser, sourceString, procDoc, 0, procDoc.getDoc().getRootElement(), tokens);
+		
+		assertEquals(10, tokens.size());
+		assertEquals("the", tokens.get(0).getValue());
+		assertEquals("quick", tokens.get(1).getValue());
+		assertEquals("methyl", tokens.get(2).getValue());
+		assertEquals("brown", tokens.get(3).getValue());
+		assertEquals("fox", tokens.get(4).getValue());
+		assertEquals("jumps", tokens.get(5).getValue());
+		assertEquals("over", tokens.get(6).getValue());
+		assertEquals("the", tokens.get(7).getValue());
+		assertEquals("chlorinated", tokens.get(8).getValue());
+		assertEquals("dog", tokens.get(9).getValue());
+	}
+	
+	@Test
+	public void testMergeNeTokens() {
+		String source = "foo ethyl acetate bar";
+		Tokeniser tokeniser = Tokeniser.getInstance();
+		IProcessingDocument procDoc = new ProcessingDocumentFactory().makeTokenisedDocument(tokeniser, source);
+		List <IToken> tokens = procDoc.getTokenSequences().get(0).getTokens();
+		assertEquals(4, tokens.size());
+		assertEquals("foo", tokens.get(0).getValue());
+		assertEquals("ethyl", tokens.get(1).getValue());
+		assertEquals("acetate", tokens.get(2).getValue());
+		assertEquals("bar", tokens.get(3).getValue());
+		
+		//FIXME manually setting the biotag is necessary as this is done by
+		//XOMBasedProcessingDocumentFactory.tokeniseOnAnnotationBoundaries
+		tokens.get(0).setBioTag("O");
+		tokens.get(1).setBioTag("B-CM");
+		tokens.get(2).setBioTag("I-CM");
+		tokens.get(3).setBioTag("O");
+		XOMBasedProcessingDocumentFactory.getInstance().mergeNeTokens(tokens, source, 0);
+		
+		assertEquals(3, tokens.size());
+		assertEquals("foo", tokens.get(0).getValue());
+		assertEquals("ethyl acetate", tokens.get(1).getValue());
+		assertEquals("bar", tokens.get(2).getValue());
+	}
+	
+	@Test
+	public void testTidyHyphensAfterNE() throws Exception{
+		InputStream in = ClassLoader.getSystemResourceAsStream("uk/ac/cam/ch/wwmm/oscar/document/sciXmlPaper.xml");
+		Document sourceDoc = new Builder().build(in);
+		Tokeniser tokeniser = Tokeniser.getInstance();
+		XOMBasedProcessingDocument procDoc = (XOMBasedProcessingDocument) XOMBasedProcessingDocumentFactory.getInstance().makeTokenisedDocument(tokeniser, sourceDoc, false, false, false);
+		List <IToken> tokens = procDoc.getTokenSequences().get(1).getTokens(); 
+		assertEquals(8, tokens.size());
+		
+		String sourceString = "the quick methylbrown fox jumps over thechlorinated dog";
+		XOMBasedProcessingDocumentFactory.getInstance().tokeniseOnAnnotationBoundaries(tokeniser, sourceString, procDoc, 0, procDoc.getDoc().getRootElement(), tokens);
+		assertEquals(10, tokens.size());
+		
+		//produces "the" "quick" "methyl" "brown" "fox" "jumps" "over" "the" "chlorinated" "dog"
+		tokens.get(3).setValue("-brown");
+		tokens.get(2).setBioTag("B-CM");
+		//produces "the" "quick" "methyl" "-brown" "fox" "jumps" "over" "the" "chlorinated" "dog"
+		
+		XOMBasedProcessingDocumentFactory.getInstance().tidyHyphensAfterNEs(tokeniser, tokens);
+		assertEquals(11, tokens.size());
+		assertEquals("the", tokens.get(0).getValue());
+		assertEquals("quick", tokens.get(1).getValue());
+		assertEquals("methyl", tokens.get(2).getValue());
+		assertEquals("-", tokens.get(3).getValue());
+		assertEquals("brown", tokens.get(4).getValue());
+		assertEquals("fox", tokens.get(5).getValue());
+		assertEquals("jumps", tokens.get(6).getValue());
+		assertEquals("over", tokens.get(7).getValue());
+		assertEquals("the", tokens.get(8).getValue());
+		assertEquals("chlorinated", tokens.get(9).getValue());
+		assertEquals("dog", tokens.get(10).getValue());
+	}
+	
+	
+	@Test
 	public void testMakeTokenSequenceStandard() throws Exception {
 		InputStream in = ClassLoader.getSystemResourceAsStream("uk/ac/cam/ch/wwmm/oscar/document/learningSpecificTokenisation.xml");
 		Document sourceDoc = new Builder().build(in);
 		Tokeniser tokeniser = Tokeniser.getInstance();
 		XOMBasedProcessingDocument procDoc = XOMBasedProcessingDocumentFactory.getInstance().makeDocument(sourceDoc);
-		Element e = (Element) sourceDoc.query("//P").get(0);
-		String text = "the quick methylbrown ethyl acetate jumps over thechlorinated dog";
+		Element para = (Element) procDoc.getDoc().query("//P").get(0);
+//		String text = "the quick methylbrown ethyl acetate jumps over thechlorinated dog";
+		String text = para.getValue();
+		int offset = Integer.parseInt(para.getAttributeValue("xtspanstart"));
 		
-		ITokenSequence tokSeq = XOMBasedProcessingDocumentFactory.getInstance().makeTokenSequence(tokeniser, false, false, null, procDoc, e, text, 0);
+		ITokenSequence tokSeq = XOMBasedProcessingDocumentFactory.getInstance().makeTokenSequence(tokeniser, false, false, null, procDoc, para, text, offset);
 		assertEquals(9, tokSeq.getTokens().size());
 		assertEquals("the", tokSeq.getTokens().get(0).getValue());
 		assertEquals("quick", tokSeq.getTokens().get(1).getValue());
@@ -87,4 +187,97 @@ public class XOMBasedProcessingDocumentFactoryTest {
 		assertEquals("dog", tokSeq.getTokens().get(9).getValue());
 	}
 	
+	
+	@Test
+	public void testTokenIds() throws Exception {
+		InputStream in = ClassLoader.getSystemResourceAsStream("uk/ac/cam/ch/wwmm/oscar/document/learningSpecificTokenisation.xml");
+		Document sourceDoc = new Builder().build(in);
+		Tokeniser tokeniser = Tokeniser.getInstance();
+		XOMBasedProcessingDocument procDoc = XOMBasedProcessingDocumentFactory.getInstance().makeDocument(sourceDoc);
+		Element e = (Element) procDoc.getDoc().getRootElement();
+		Element para = (Element) procDoc.getDoc().query("//P").get(0); 
+		String text = para.getValue();
+		int offset = Integer.parseInt(para.getAttributeValue("xtspanstart"));
+		
+		ITokenSequence tokSeq = XOMBasedProcessingDocumentFactory.getInstance().makeTokenSequence(tokeniser, false, false, null, procDoc, e, text, offset);
+		assertEquals(9, tokSeq.getTokens().size());
+		for (int i = 0; i < tokSeq.size(); i++) {
+			assertEquals(i, tokSeq.getTokens().get(i).getId());
+		}
+		
+		tokSeq = XOMBasedProcessingDocumentFactory.getInstance().makeTokenSequence(tokeniser, true, false, null, procDoc, e, text, offset);
+		assertEquals(11, tokSeq.getTokens().size());
+		for (int i = 0; i < tokSeq.size(); i++) {
+			assertEquals(i, tokSeq.getTokens().get(i).getId());
+		}
+		
+		tokSeq = XOMBasedProcessingDocumentFactory.getInstance().makeTokenSequence(tokeniser, true, true, null, procDoc, e, text, offset);
+		assertEquals(10, tokSeq.getTokens().size());
+		for (int i = 0; i < tokSeq.size(); i++) {
+			assertEquals(i, tokSeq.getTokens().get(i).getId());
+		}
+	}
+	
+	@Test
+	public void testTokenIndex() throws Exception {
+		InputStream in = ClassLoader.getSystemResourceAsStream("uk/ac/cam/ch/wwmm/oscar/document/learningSpecificTokenisation.xml");
+		Document sourceDoc = new Builder().build(in);
+		Tokeniser tokeniser = Tokeniser.getInstance();
+		XOMBasedProcessingDocument procDoc = XOMBasedProcessingDocumentFactory.getInstance().makeDocument(sourceDoc);
+		//fields aren't initialised by above call
+		procDoc.tokensByStart = new HashMap<Integer,IToken>();
+		procDoc.tokensByEnd = new HashMap<Integer,IToken>();
+		Element e = (Element) procDoc.getDoc().getRootElement();
+		Element para = (Element) procDoc.getDoc().query("//P").get(0); 
+		String text = para.getValue();
+		int offset = Integer.parseInt(para.getAttributeValue("xtspanstart"));
+		
+		List <IToken> tokens = XOMBasedProcessingDocumentFactory.getInstance().makeTokenSequence(tokeniser, false, false, null, procDoc, e, text, offset).getTokens();
+		assertEquals(9, procDoc.getTokensByStart().size());
+		assertEquals(9, procDoc.getTokensByEnd().size());
+		for (IToken iToken : tokens) {
+			assertNotNull(iToken);
+			assertTrue(iToken == procDoc.getTokensByStart().get(iToken.getStart()));
+			assertTrue(iToken == procDoc.getTokensByEnd().get(iToken.getEnd()));
+		}
+		
+		tokens = XOMBasedProcessingDocumentFactory.getInstance().makeTokenSequence(tokeniser, true, false, null, procDoc, e, text, offset).getTokens();
+		assertEquals(11, procDoc.getTokensByStart().size());
+		assertEquals(11, procDoc.getTokensByEnd().size());
+		for (IToken iToken : tokens) {
+			assertNotNull(iToken);
+			assertTrue(iToken == procDoc.getTokensByStart().get(iToken.getStart()));
+			assertTrue(iToken == procDoc.getTokensByEnd().get(iToken.getEnd()));
+		}
+		
+		tokens = XOMBasedProcessingDocumentFactory.getInstance().makeTokenSequence(tokeniser, true, true, null, procDoc, e, text, offset).getTokens();
+		assertEquals(10, procDoc.getTokensByStart().size());
+		assertEquals(10, procDoc.getTokensByEnd().size());
+		for (IToken iToken : tokens) {
+			assertNotNull(iToken);
+			assertTrue(iToken == procDoc.getTokensByStart().get(iToken.getStart()));
+			assertTrue(iToken == procDoc.getTokensByEnd().get(iToken.getEnd()));
+		}
+	}
+	
+	@Test
+	public void testTokenSequenceAssignments() throws Exception {
+		InputStream in = ClassLoader.getSystemResourceAsStream("uk/ac/cam/ch/wwmm/oscar/document/learningSpecificTokenisation.xml");
+		Document sourceDoc = new Builder().build(in);
+		Tokeniser tokeniser = Tokeniser.getInstance();
+		XOMBasedProcessingDocument procDoc = XOMBasedProcessingDocumentFactory.getInstance().makeDocument(sourceDoc);
+		//FIXME fields aren't initialised by above call... yet
+		procDoc.tokensByStart = new HashMap<Integer,IToken>();
+		procDoc.tokensByEnd = new HashMap<Integer,IToken>();
+		Element e = (Element) procDoc.getDoc().getRootElement();
+		Element para = (Element) procDoc.getDoc().query("//P").get(0); 
+		String text = para.getValue();
+		int offset = Integer.parseInt(para.getAttributeValue("xtspanstart"));
+		
+		ITokenSequence ts = XOMBasedProcessingDocumentFactory.getInstance().makeTokenSequence(tokeniser, false, false, null, procDoc, e, text, offset);
+		for (IToken token : ts.getTokens()) {
+			assertTrue(ts == token.getTokenSequence());
+		}
+		assertTrue(e == ((TokenSequence)ts).getElem());
+	}
 }
--- oscar4-tokeniser/src/main/java/uk/ac/cam/ch/wwmm/oscartokeniser/Tokeniser.java
+++ oscar4-tokeniser/src/main/java/uk/ac/cam/ch/wwmm/oscartokeniser/Tokeniser.java
@@ -175,38 +175,13 @@ public final class Tokeniser implements ITokeniser {
 		}
 		tokens = tmpTokens;
 		
-		if (annotations != null && tokeniseForNEs) {
-			modifyTokenisationForTraining(s, doc, offset, annotations,
-					mergeNEs, tokens);
-		}
-
 		TokenSequence tokenSequence = indexTokensAndMakeTokenSequence(s, doc,
 				offset, annotations, tokens);
 		
 		return tokenSequence;
 	}
 
-	private void modifyTokenisationForTraining(String s,
-			IProcessingDocument doc, int offset, Element annotations,
-			boolean mergeNEs, List<IToken> tokens) {
-		
-		try {
-			/*
-			 * @lh359: This function is called
-			 * when we know the tag of the word
-			 * This is what was editing the results
-			 * in oscarCRF
-			 * 
-			 ***************************/
-			tokeniseOnAnnotationBoundaries(s, doc, offset, annotations, tokens);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		tidyHyphensAfterNEs(tokens);
-		if (mergeNEs) {
-			mergeNeTokens(tokens, s, offset);
-		}
-	}
+	
 
 	public TokenSequence indexTokensAndMakeTokenSequence(String s,
 			IProcessingDocument doc, int offset, Element annotations,
@@ -491,7 +466,7 @@ public final class Tokeniser implements ITokeniser {
 		}
 	}
 
-	private List<IToken> splitAt(IToken token, int splitOffset) {
+	public List<IToken> splitAt(IToken token, int splitOffset) {
 		int internalOffset = splitOffset - token.getStart();
 		List<IToken> tokens = new LinkedList<IToken>();
 		tokens
@@ -523,210 +498,4 @@ public final class Tokeniser implements ITokeniser {
 	}
 
 	
-	public void tokeniseOnAnnotationBoundaries(String sourceString, IProcessingDocument doc,
-			int offset, Element safOrInlineAnnotations, List<IToken> tokens) throws Exception {
-		Nodes annotationNodes;
-		int currentNodeId = 0;
-		Element currentElem = null;
-		int elemStart = -1;
-		int elemEnd = -1;
-		String neType = null;
-		boolean sourceIsInline;
-
-		//prime variables, depending on whether we are working from saf or inline
-		if (safOrInlineAnnotations.getLocalName().equals("saf")) {
-			sourceIsInline = false;
-			Nodes safAnnotationNodes = safOrInlineAnnotations.query("annot");
-			annotationNodes = new Nodes();
-			int endOffset = sourceString.length() + offset;
-			for (int i = 0; i < safAnnotationNodes.size(); i++) {
-				Element annot = (Element) safAnnotationNodes.get(i);
-				String startX = annot.getAttributeValue("from");
-				int start = doc.getStandoffTable().getOffsetAtXPoint(startX);
-				if (start < offset)
-					continue;
-				String endX = annot.getAttributeValue("to");
-				int end = doc.getStandoffTable().getOffsetAtXPoint(endX);
-				if (end > endOffset)
-					continue;
-				annotationNodes.append(annot);
-			}
-			if (annotationNodes.size() == 0)
-				return;
-			currentElem = (Element) annotationNodes.get(currentNodeId);
-			elemStart = doc.getStandoffTable().getOffsetAtXPoint(currentElem
-					.getAttributeValue("from"));
-			elemEnd = doc.getStandoffTable().getOffsetAtXPoint(currentElem
-					.getAttributeValue("to"));
-		}
-		else {
-			sourceIsInline = true;
-			annotationNodes = safOrInlineAnnotations.query(".//ne");
-			if (annotationNodes.size() == 0)
-				return;
-			currentElem = (Element) annotationNodes.get(currentNodeId);
-			elemStart = Integer.parseInt(currentElem
-					.getAttributeValue("xtspanstart"));
-			elemEnd = Integer.parseInt(currentElem
-					.getAttributeValue("xtspanend"));
-			neType = currentElem.getAttributeValue("type");
-		}
-		
-		
-		//find start and end offsets the first annotation that fits the condition elemEnd > elemStart 
-		while (!(elemEnd > elemStart)) {
-			currentNodeId++;
-			if (currentNodeId >= annotationNodes.size()) {
-				return;
-			}
-			else {
-				currentElem = (Element) annotationNodes.get(currentNodeId);
-				if (sourceIsInline) {
-					elemStart = Integer.parseInt(currentElem
-							.getAttributeValue("xtspanstart"));
-					elemEnd = Integer.parseInt(currentElem
-							.getAttributeValue("xtspanend"));
-					neType = currentElem.getAttributeValue("type");
-				} else {
-					elemStart = doc.getStandoffTable().getOffsetAtXPoint(currentElem
-							.getAttributeValue("from"));
-					elemEnd = doc.getStandoffTable().getOffsetAtXPoint(currentElem
-							.getAttributeValue("to"));
-				}
-			}
-		}
-		
-		
-		//split tokens that span annotation boundaries on those annotation boundaries
-		int splits = 0;
-		int i = 0;
-		boolean inElem = false;
-		while (i < tokens.size()) {
-			//note that when we split tokens we don't increment i, so we're
-			//doing that recursive processing without recursion thing again
-			if (!inElem) {
-				if (tokens.get(i).getEnd() <= elemStart) {
-					//token precedes annotation element - skip
-					i++;
-				}
-				else if (tokens.get(i).getStart() >= elemStart
-						&& tokens.get(i).getEnd() <= elemEnd) {
-					//token is fully contained within annotation
-					tokens.get(i).setBioTag("B-" + neType);
-					((Token)tokens.get(i)).setNeElem(currentElem);
-					inElem = true;
-					i++;
-				}
-				else if (tokens.get(i).getStart() < elemStart) {
-					//token straddles beginning of annotation
-					splits++;
-					List<IToken> splitResults = splitAt(tokens.get(i), elemStart);
-					tokens.remove(i);
-					tokens.addAll(i, splitResults);
-				}
-				else if (tokens.get(i).getEnd() > elemEnd) {
-					//token straddles end of annotation
-					splits++;
-					List<IToken> splitResults = splitAt(tokens.get(i), elemEnd);
-					tokens.remove(i);
-					tokens.addAll(i, splitResults);
-				}
-				else {
-					//something has probably gone wrong
-					i++;
-				}
-			}
-			else {
-				if (tokens.get(i).getStart() >= elemEnd) {
-					//token starts after the annotation - find next annotation
-					inElem = false;
-					int oldElemEnd = elemEnd;
-					while (!(elemEnd > oldElemEnd && elemEnd > elemStart)) {
-						currentNodeId++;
-						if (currentNodeId >= annotationNodes.size()) {
-							return;
-						} else {
-							currentElem = (Element) annotationNodes.get(currentNodeId);
-							if (sourceIsInline) {
-								elemStart = Integer.parseInt(currentElem
-										.getAttributeValue("xtspanstart"));
-								elemEnd = Integer.parseInt(currentElem
-										.getAttributeValue("xtspanend"));
-								neType = currentElem.getAttributeValue("type");
-							} else {
-								elemStart = doc.getStandoffTable()
-										.getOffsetAtXPoint(currentElem
-												.getAttributeValue("from"));
-								elemEnd = doc.getStandoffTable()
-										.getOffsetAtXPoint(currentElem
-												.getAttributeValue("to"));
-							}
-						}
-					}
-				}
-				else if (tokens.get(i).getEnd() <= elemEnd) {
-					//token is contained in the annotation
-					tokens.get(i).setBioTag("I-" + neType);
-					((Token)tokens.get(i)).setNeElem(currentElem);
-					i++;
-				} else {
-					//token straddles the end of the annotation
-					splits++;
-					List<IToken> splitResults = splitAt(tokens.get(i), elemEnd);
-					tokens.remove(i);
-					tokens.addAll(i, splitResults);
-				}
-			}
-		}
-	}
-
-	public void tidyHyphensAfterNEs(List<IToken> tokens) {
-		int i = 0;
-		String prevTokType = "O";
-		while (i < tokens.size()) {
-			if (prevTokType.equals("O")
-					|| !tokens.get(i).getBioTag().equals("O")
-					|| tokens.get(i).getValue().length() < 2
-					|| !StringTools.isHyphen(tokens.get(i).getValue()
-							.substring(0, 1))
-					|| !(tokens.get(i).getStart() == tokens.get(i - 1).getEnd())) {
-				i++;
-				prevTokType = tokens.get(i - 1).getBioTag();
-			} else {
-				List<IToken> splitResults = splitAt(tokens.get(i),
-						tokens.get(i).getStart() + 1);
-				tokens.remove(i);
-				tokens.addAll(i, splitResults);
-				i += 2;
-			}
-		}
-	}
-
-	//FIXME I'm not sure that this method does what one would expect...
-	public void mergeNeTokens(List<IToken> tokens, String sourceString,
-			int offset) {
-		List<IToken> newTokens = new ArrayList<IToken>();
-		IToken currentToken = null;
-		String neClass = null;
-		for (IToken t : tokens) {
-			if (currentToken == null || neClass == null
-					|| t.getBioTag().startsWith("O") || t.getBioTag().startsWith("B")) {
-				currentToken = t;
-				newTokens.add(t);
-				if (currentToken.getBioTag().equals("O")) {
-					neClass = null;
-				} else {
-					neClass = currentToken.getBioTag().substring(2);
-				}
-				// inside
-			} else {
-				currentToken.setEnd(t.getEnd());
-				currentToken.setValue(sourceString.substring(currentToken.getStart()
-						- offset, currentToken.getEnd() - offset));
-			}
-
-		}
-		tokens.clear();
-		tokens.addAll(newTokens);
-	}
 }

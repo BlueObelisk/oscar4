4f5a4683d6343fe2c107443ddb0f17f26fd2cce9
Egon Willighagen <egonw@users.noreply.github.com>
2010-12-01 14:23:28 +0100

    MEMMTrainer now also uses MEMMModel, via MutableMEMMModel

--- oscar4-memmrecogniser-train/src/main/java/uk/ac/cam/ch/wwmm/oscarMEMM/memm/data/MutableMEMMModel.java
+++ oscar4-memmrecogniser-train/src/main/java/uk/ac/cam/ch/wwmm/oscarMEMM/memm/data/MutableMEMMModel.java
@@ -1,5 +1,6 @@
 package uk.ac.cam.ch.wwmm.oscarMEMM.memm.data;
 
+import opennlp.maxent.GISModel;
 import uk.ac.cam.ch.wwmm.oscarMEMM.memm.rescorer.MEMMOutputRescorer;
 
 public class MutableMEMMModel extends MEMMModel {
@@ -8,4 +9,19 @@ public class MutableMEMMModel extends MEMMModel {
 		super.rescorer = rescorer;
 	}
 
+	public void makeEntityTypesAndZeroProbs() {
+		super.makeEntityTypesAndZeroProbs();
+	}
+
+	public void setUberModel(GISModel trainModel) {
+		super.ubermodel = trainModel;
+	}
+	
+	public void putGISModel(String prev, GISModel gisModel) {
+		super.gmByPrev.put(prev, gisModel);
+	}
+
+	public void addTag(String prev) {
+		super.tagSet.add(prev);
+	}
 }

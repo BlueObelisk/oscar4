dd97914ac1d7717f718065d20ce145f240381285
Sam Adams <seadams@gmail.com>
2010-12-16 08:36:35 +0000

    Re-indented code in TermMaps

--- oscar4-obo/src/main/java/uk/ac/cam/ch/wwmm/oscar/obo/TermMaps.java
+++ oscar4-obo/src/main/java/uk/ac/cam/ch/wwmm/oscar/obo/TermMaps.java
@@ -3,7 +3,6 @@ package uk.ac.cam.ch.wwmm.oscar.obo;
 import org.apache.log4j.Logger;
 import uk.ac.cam.ch.wwmm.oscar.tools.OscarProperties;
 import uk.ac.cam.ch.wwmm.oscar.tools.ResourceGetter;
-import uk.ac.cam.ch.wwmm.oscar.tools.StringTools;
 import uk.ac.cam.ch.wwmm.oscar.types.NamedEntityType;
 
 import java.io.BufferedReader;
@@ -14,8 +13,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**A class to hold several mappings between terms and their identifiers.
  *
@@ -24,7 +21,7 @@ import java.util.regex.Pattern;
  */
 public final class TermMaps {
 
-	private static final Logger logger = Logger.getLogger(TermMaps.class);
+    private static final Logger logger = Logger.getLogger(TermMaps.class);
 
     private static final ResourceGetter RESOURCE_GETTER = new ResourceGetter("/");
 
@@ -34,61 +31,61 @@ public final class TermMaps {
     private static final String STRUCTURE_TYPES_TERMS_FILE = "uk/ac/cam/ch/wwmm/oscar/obo/terms/structureTypes.txt";
     private static final String CUST_ENT_TERMS_FILE = "uk/ac/cam/ch/wwmm/oscar/obo/terms/custEnt.txt";
 
-	private Map<String, NamedEntityType> neTerms;
-	private Map<String, String> iePatterns;
-	private Map<String, String> custEnt;
-	private Map<String, String> structureTypes;
-	private Set<String> suffixes;
+    private Map<String, NamedEntityType> neTerms;
+    private Map<String, String> iePatterns;
+    private Map<String, String> custEnt;
+    private Map<String, String> structureTypes;
+    private Set<String> suffixes;
 
-	private static TermMaps myInstance;
+    private static TermMaps myInstance;
 
 
     /**Initialise the TermMaps singleton, deleting the old one if one already
-	 * exists.
-	 *
-	 * @throws Exception
-	 */
-	public static void reinitialise() {
-		myInstance = null;
-		getInstance();
-	}
+     * exists.
+     *
+     * @throws Exception
+     */
+    public static void reinitialise() {
+        myInstance = null;
+        getInstance();
+    }
 
-	/**Initialise the TermMaps singleton, if this has not already been done.
-	 *
-	 */
-	public static void init() {
-		try {
-			if(myInstance == null) {
-				myInstance = new TermMaps();
-			}
-		} catch (Exception e) {
-			throw new Error(e);
-		}
-	}
+    /**Initialise the TermMaps singleton, if this has not already been done.
+     *
+     */
+    public static void init() {
+        try {
+            if(myInstance == null) {
+                myInstance = new TermMaps();
+            }
+        } catch (Exception e) {
+            throw new Error(e);
+        }
+    }
 
-	private static TermMaps getInstance() {
-		try {
-		if(myInstance == null) {
-			myInstance = new TermMaps();
-		}
-		return myInstance;
-		} catch (Exception e) {
-			throw new Error(e);
-		}
-	}
+    private static TermMaps getInstance() {
+        try {
+            if(myInstance == null) {
+                myInstance = new TermMaps();
+            }
+            return myInstance;
+        } catch (Exception e) {
+            throw new Error(e);
+        }
+    }
 
-	
 
-	private void digestSuffixes() {
-		suffixes = new HashSet<String>();
-		for(String s : neTerms.keySet()) {
-			String [] ss = s.split("\\s+");
-			for (int i = 0; i < ss.length; i++) {
-				if(ss[i].startsWith("$-")) {
-					suffixes.add(ss[i].substring(2));
-				}
-			}
-		}
+
+    private void digestSuffixes() {
+        suffixes = new HashSet<String>();
+        for(String s : neTerms.keySet()) {
+            String [] ss = s.split("\\s+");
+            for (int i = 0; i < ss.length; i++) {
+                if(ss[i].startsWith("$-")) {
+                    suffixes.add(ss[i].substring(2));
+                }
+            }
+        }
     }
 
     private Map<String, String> loadTerms(String path, boolean concatenateTypes) throws IOException {
@@ -110,60 +107,61 @@ public final class TermMaps {
         return neTermMap;
     }
 
-	private TermMaps() throws IOException {
-		logger.debug("Initialising term maps... ");
-		neTerms = getNeTermMap(NE_TERMS_FILE, false);
-		//add additional neTerms for polymers if set to polymer mode
-		if (OscarProperties.getData().polymerMode) {
-			Map <String, NamedEntityType> polyNeTerms = getNeTermMap(POLY_NE_TERMS_FILE, false);
-			neTerms.putAll(polyNeTerms);
-		}
-		iePatterns = loadTerms(IE_PATTERNS_TERMS_FILE, false);
-		structureTypes = loadTerms(STRUCTURE_TYPES_TERMS_FILE, false);
-		custEnt = loadTerms(CUST_ENT_TERMS_FILE, true);
-		digestSuffixes();
-		logger.debug("term maps initialised");
-	}
+    private TermMaps() throws IOException {
+        logger.debug("Initialising term maps... ");
+        neTerms = getNeTermMap(NE_TERMS_FILE, false);
+        //add additional neTerms for polymers if set to polymer mode
+        if (OscarProperties.getData().polymerMode) {
+            Map <String, NamedEntityType> polyNeTerms = getNeTermMap(POLY_NE_TERMS_FILE, false);
+            neTerms.putAll(polyNeTerms);
+        }
+        iePatterns = loadTerms(IE_PATTERNS_TERMS_FILE, false);
+        structureTypes = loadTerms(STRUCTURE_TYPES_TERMS_FILE, false);
+        custEnt = loadTerms(CUST_ENT_TERMS_FILE, true);
+        digestSuffixes();
+        logger.debug("term maps initialised");
+    }
 
 
 
     /**Gets the term map for neTerms.txt.
-	 *
-	 * @return The term map.
-	 */
-	public static Map<String, NamedEntityType> getNeTerms() {
-		return getInstance().neTerms;
-	}
+     *
+     * @return The term map.
+     */
+    public static Map<String, NamedEntityType> getNeTerms() {
+        return getInstance().neTerms;
+    }
 
-	/**Gets the term map for iePatterns.txt.
-	 *
-	 * @return The term map.
-	 */
-	public static Map<String, String> getIePatterns() {
-		return getInstance().iePatterns;
-	}
+    /**Gets the term map for iePatterns.txt.
+     *
+     * @return The term map.
+     */
+    public static Map<String, String> getIePatterns() {
+        return getInstance().iePatterns;
+    }
 
-	/**Gets the term map for custEnt.txt.
-	 *
-	 * @return The term map.
-	 */
-	public static Map<String, String> getCustEnt() {
-		return getInstance().custEnt;
-	}
+    /**Gets the term map for custEnt.txt.
+     *
+     * @return The term map.
+     */
+    public static Map<String, String> getCustEnt() {
+        return getInstance().custEnt;
+    }
 
-	/**Gets the term map for structureTypes.txt.
-	 *
-	 * @return The term map.
-	 */
-	public static Map<String, String> getStructureTypes() {
-		return getInstance().structureTypes;
-	}
+    /**Gets the term map for structureTypes.txt.
+     *
+     * @return The term map.
+     */
+    public static Map<String, String> getStructureTypes() {
+        return getInstance().structureTypes;
+    }
 
-	/**Gets a collection of suffixes harvested from neTerms.txt.
-	 *
-	 * @return A collection of suffixes harvested from neTerms.txt
-	 */
-	public static Set<String> getSuffixes() {
-		return getInstance().suffixes;
-	}
+    /**Gets a collection of suffixes harvested from neTerms.txt.
+     *
+     * @return A collection of suffixes harvested from neTerms.txt
+     */
+    public static Set<String> getSuffixes() {
+        return getInstance().suffixes;
+    }
+
 }

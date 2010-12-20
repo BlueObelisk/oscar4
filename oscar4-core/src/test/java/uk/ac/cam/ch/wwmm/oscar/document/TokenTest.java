fee18eb53686ba62b7cb350dc0c496e5b83e066d
Egon Willighagen <egonw@users.noreply.github.com>
2010-12-20 15:45:52 +0100

    Added unit tests

---
+++ oscar4-core/src/test/java/uk/ac/cam/ch/wwmm/oscar/document/TokenTest.java
@@ -0,0 +1,53 @@
+package uk.ac.cam.ch.wwmm.oscar.document;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TokenTest {
+
+	@Test
+	public void testValue() {
+		Token token = new Token(
+			"alsoAToken", 0, 0, null, null, null
+		);
+		Assert.assertEquals("alsoAToken", token.getValue());
+		token.setValue("token");
+		Assert.assertEquals("token", token.getValue());
+	}
+
+	@Test
+	public void testId() {
+		Token token = new Token(
+			null, 0, 0, null, null, null
+		);
+		token.setId(777);
+		Assert.assertEquals(777, token.getId());
+	}
+
+	@Test
+	public void testStart() {
+		Token token = new Token(
+			null, 77, 0, null, null, null
+		);
+		Assert.assertEquals(77, token.getStart());
+	}
+
+	@Test
+	public void testEnd() {
+		Token token = new Token(
+			null, 0, 77, null, null, null
+		);
+		Assert.assertEquals(77, token.getEnd());
+		token.setEnd(88);
+		Assert.assertEquals(88, token.getEnd());
+	}
+
+	@Test
+	public void testBioTag() {
+		Token token = new Token(
+			null, 0, 77, null, null, null
+		);
+		token.setBioTag("CM");
+		Assert.assertEquals("CM", token.getBioTag());
+	}
+}

6e957027a6944e675a88fbaacd1e4a370dcec4bf
Egon Willighagen <egonw@users.noreply.github.com>
2010-12-30 17:11:26 +0100

    Factored out the sentence split tokens into a separate class

---
+++ oscar4-core/src/main/java/uk/ac/cam/ch/wwmm/oscar/document/SentenceSplitTokens.java
@@ -0,0 +1,23 @@
+package uk.ac.cam.ch.wwmm.oscar.document;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SentenceSplitTokens {
+
+	private final Set<String> splitTokens =
+		new HashSet<String>(Arrays.asList(
+			".",
+		    "?",
+		    "!",
+		    "\""
+		));
+
+	/**
+	 * Returns true if the token is recognized as a token splitting a sentence.
+	 */
+	public boolean contains(String token) {
+		return splitTokens.contains(token);
+	}
+}

9c6055ec7b110e7e70c8f189a7e5b123ec2347fe
dmj30 <dmj30@localhost>
2011-01-31 17:42:30 +0000

    Renamed Oscar4RegressionTest so that it would be run by maven, and fixed various bugs it highlighted.

--- oscar4-recogniser-core/src/main/java/uk/ac/cam/ch/wwmm/oscarrecogniser/tokenanalysis/PrefixFinder.java
+++ oscar4-recogniser-core/src/main/java/uk/ac/cam/ch/wwmm/oscarrecogniser/tokenanalysis/PrefixFinder.java
@@ -1,43 +1,67 @@
 package uk.ac.cam.ch.wwmm.oscarrecogniser.tokenanalysis;
 
-import java.util.Set;
 import java.util.regex.Pattern;
 
 import uk.ac.cam.ch.wwmm.oscar.tools.StringTools;
 import uk.ac.cam.ch.wwmm.oscarrecogniser.manualAnnotations.ManualAnnotations;
 
+/**
+ * 
+ * @author ptc24
+ * @author dmj30
+ *
+ */
 public class PrefixFinder {
-	private static String primesRe = "[" + StringTools.primes + "]*";
-	private static String locantRe = "(\\d+" + primesRe + "[RSEZDLH]?|" +
+	private static String primesRegex = "[" + StringTools.primes + "]*";
+	private static String locantRegex = "(\\d+" + primesRegex + "[RSEZDLH]?|" +
 	"\\(([RSEZDLH\u00b1]|\\+|" + StringTools.hyphensRegex + ")\\)|" +
-		"[DLRSEZ]|" +
+		"[DLRSEZ]|" +	//as in "D/L", "R/S" and "E/Z"
 			"([CNOS]|Se)\\d*|" +
 			"\\d*[" + StringTools.lowerGreek + "]|" +
 	"cis|trans|o(rtho)?|m(eta)?|p(ara)?|asym|sym|sec|tert|catena|closo|enantio|ent|endo|exo|" +
 	"fac|mer|gluco|nido|aci|erythro|threo|arachno|meso|syn|anti|tele|cine" +
-	")" + primesRe;
-	private static String prefixRe = "(" + locantRe + "(," + locantRe + ")*)";
+	")" + primesRegex;
+	private static String prefixRegex = "(" + locantRegex + "(," + locantRegex + ")*)";
 	
-	public static Pattern prefixPattern = Pattern.compile(prefixRe + 
+	public static Pattern prefixPattern = Pattern.compile(prefixRegex + 
 					"[" + StringTools.hyphens + "](\\S*)");
 	
-	public static Pattern prefixBody = Pattern.compile(prefixRe);
+	public static Pattern prefixBody = Pattern.compile(prefixRegex);
 
+	
+	/**
+	 * Determines the chemical prefix at the start of the specified word
+	 * 
+	 * @param s the word to be checked for a prefix
+	 * @param manualAnnotations
+	 * @return the String corresponding to the chemical prefix or null
+	 * if none was found
+	 */
 	public static String getPrefix(String s, ManualAnnotations manualAnnotations) {
 		if(prefixPattern.matcher(s).matches()) {
-			int idx = s.indexOf("-");
+			int idx = findIndexOfHyphen(s);
 			// Check if it's a not-splitting word
 			if (manualAnnotations != null) {
 				if(manualAnnotations.getNotForPrefix().contains(s.substring(idx+1))) {
 					return null;
 				}
-				if(idx == 0) {
-					return null;
-				}	
 			}
+			if(idx == 0) {
+				return null;
+			}	
 			return s.substring(0, idx+1);
 		}
 		return null;		
+	}
+
+	private static int findIndexOfHyphen(String s) {
+		for (int i = 0; i < StringTools.hyphens.length(); i++) {
+			int idx = s.indexOf(StringTools.hyphens.charAt(i));
+			if (idx != -1) {
+				return idx;
+			}
+		}
+		return -1;
 	}
 
 	//TODO redirect callers to getPrefix(String, ManualAnnotations)
---
+++ oscar4-recogniser-core/src/test/java/uk/ac/cam/ch/wwmm/oscarrecogniser/finder/DFAONTCPRFinderTest.java
@@ -0,0 +1,195 @@
+package uk.ac.cam.ch.wwmm.oscarrecogniser.finder;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import uk.ac.cam.ch.wwmm.oscar.document.IToken;
+import uk.ac.cam.ch.wwmm.oscar.document.ITokenSequence;
+import uk.ac.cam.ch.wwmm.oscar.document.NamedEntity;
+import uk.ac.cam.ch.wwmm.oscar.document.Token;
+import uk.ac.cam.ch.wwmm.oscar.document.TokenSequence;
+import uk.ac.cam.ch.wwmm.oscar.types.NamedEntityType;
+import uk.ac.cam.ch.wwmm.oscartokeniser.Tokeniser;
+
+/**
+ * 
+ * @author dmj30
+ *
+ */
+public class DFAONTCPRFinderTest {
+
+    @Test
+    public void testFindNumericalChemicalPrefixes() {
+    	String text = "The 1- and 2-foo bar";
+    	ITokenSequence tokenSequence = Tokeniser.getInstance().tokenise(text);
+    	List <NamedEntity> nes = DFAONTCPRFinder.getInstance().findNamedEntities(tokenSequence);
+    	assertTrue(toSurfaceList(nes).contains("2-"));
+    	assertTrue(toSurfaceList(nes).contains("1-"));
+    	assertEquals(2, nes.size());
+    }
+    
+    @Test
+    public void testFindComplexNumericalChemicalPrefixes() {
+    	String text = "The 1,2- and 2,3-foo bar";
+    	ITokenSequence tokenSequence = Tokeniser.getInstance().tokenise(text);
+    	List <NamedEntity> nes = DFAONTCPRFinder.getInstance().findNamedEntities(tokenSequence);
+    	assertTrue(toSurfaceList(nes).contains("2,3-"));
+    	assertTrue(toSurfaceList(nes).contains("1,2-"));
+    	assertEquals(2, nes.size());
+    }
+    
+    @Test
+    public void testFindWordyChemicalPrefixes() {
+    	String text = "The cis- and trans-foo bar";
+    	ITokenSequence tokenSequence = Tokeniser.getInstance().tokenise(text);
+    	List <NamedEntity> nes = DFAONTCPRFinder.getInstance().findNamedEntities(tokenSequence);
+    	assertTrue(toSurfaceList(nes).contains("trans-"));
+    	assertTrue(toSurfaceList(nes).contains("cis-"));
+    	assertEquals(2, nes.size());
+    }
+    
+    @Test
+    public void testDontFindNonChemicalPrefixes() {
+    	String text = "The foo- and bar-foobar";
+    	ITokenSequence tokenSequence = Tokeniser.getInstance().tokenise(text);
+    	List <NamedEntity> nes = DFAONTCPRFinder.getInstance().findNamedEntities(tokenSequence);
+    	assertEquals(0, nes.size());
+    }
+    
+    private List <String> toSurfaceList(List <NamedEntity> nes) {
+    	List <String> surfaces = new ArrayList<String>();
+    	for (NamedEntity ne : nes) {
+			surfaces.add(ne.getSurface());
+		}
+    	return surfaces;
+	}
+	
+    
+    @Test
+    public void testHandleTokenForNumericalPrefix() {
+    	NECollector collector = new NECollector();
+    	assertEquals(0, collector.getNes().size());
+    	
+    	Token t = new Token("1,2-", 0, 4, null, null, null);
+    	List <IToken> tokens = new ArrayList<IToken>();
+    	tokens.add(t);
+    	TokenSequence tokSeq = new TokenSequence("1,2-", 0, null, tokens);
+    	t.setTokenSequence(tokSeq);
+    	
+    	DFAONTCPRFinder.getInstance().handleTokenForPrefix(t, collector);
+    	List <NamedEntity> nes = collector.getNes(); 
+    	assertEquals(1, nes.size());
+    	assertEquals("1,2-", nes.get(0).getSurface());
+    	assertTrue(NamedEntityType.LOCANTPREFIX == nes.get(0).getType());
+    }
+    
+    @Test
+    public void testHandleTokenForEmbeddedNumericalPrefix() {
+    	NECollector collector = new NECollector();
+    	assertEquals(0, collector.getNes().size());
+    	
+    	Token t = new Token("2,3-substituted", 0, 15, null, null, null);
+    	List <IToken> tokens = new ArrayList<IToken>();
+    	tokens.add(t);
+    	TokenSequence tokSeq = new TokenSequence("2,3-substituted", 0, null, tokens);
+    	t.setTokenSequence(tokSeq);
+    	
+    	DFAONTCPRFinder.getInstance().handleTokenForPrefix(t, collector);
+    	List <NamedEntity> nes = collector.getNes(); 
+    	assertEquals(1, nes.size());
+    	assertEquals("2,3-", nes.get(0).getSurface());
+    	assertTrue(NamedEntityType.LOCANTPREFIX == nes.get(0).getType());
+    }
+    
+    @Test
+    public void testHandleTokenForTokenisedNumericalPrefix() {
+    	NECollector collector = new NECollector();
+    	assertEquals(0, collector.getNes().size());
+    	
+    	Token t1 = new Token("2,3", 0, 3, null, null, null);
+    	t1.setId(0);
+    	Token t2 = new Token("-", 3, 4, null, null, null);
+    	t2.setId(1);
+    	List <IToken> tokens = new ArrayList<IToken>();
+    	tokens.add(t1);
+    	tokens.add(t2);
+    	TokenSequence tokSeq = new TokenSequence("2,3-", 0, null, tokens);
+    	t1.setTokenSequence(tokSeq);
+    	t2.setTokenSequence(tokSeq);
+    	
+    	DFAONTCPRFinder.getInstance().handleTokenForPrefix(t1, collector);
+    	assertEquals(0, collector.getNes().size());
+    	
+    	DFAONTCPRFinder.getInstance().handleTokenForPrefix(t2, collector);
+    	List <NamedEntity> nes = collector.getNes(); 
+    	assertEquals(1, nes.size());
+    	assertEquals("2,3-", nes.get(0).getSurface());
+    	assertTrue(NamedEntityType.LOCANTPREFIX == nes.get(0).getType());
+    }
+    
+    @Test
+    public void testHandleTokenForWordyPrefix() {
+    	NECollector collector = new NECollector();
+    	assertEquals(0, collector.getNes().size());
+    	
+    	Token t = new Token("endo-", 0, 5, null, null, null);
+    	List <IToken> tokens = new ArrayList<IToken>();
+    	tokens.add(t);
+    	TokenSequence tokSeq = new TokenSequence("endo-", 0, null, tokens);
+    	t.setTokenSequence(tokSeq);
+    	
+    	DFAONTCPRFinder.getInstance().handleTokenForPrefix(t, collector);
+    	List <NamedEntity> nes = collector.getNes(); 
+    	assertEquals(1, nes.size());
+    	assertEquals("endo-", nes.get(0).getSurface());
+    	assertTrue(NamedEntityType.LOCANTPREFIX == nes.get(0).getType());
+    }
+    
+    @Test
+    public void testHandleTokenForEmbeddedWordyPrefix() {
+    	NECollector collector = new NECollector();
+    	assertEquals(0, collector.getNes().size());
+    	
+    	Token t = new Token("trans-isomer", 0, 12, null, null, null);
+    	List <IToken> tokens = new ArrayList<IToken>();
+    	tokens.add(t);
+    	TokenSequence tokSeq = new TokenSequence("trans-isomer", 0, null, tokens);
+    	t.setTokenSequence(tokSeq);
+    	
+    	DFAONTCPRFinder.getInstance().handleTokenForPrefix(t, collector);
+    	List <NamedEntity> nes = collector.getNes(); 
+    	assertEquals(1, nes.size());
+    	assertEquals("trans-", nes.get(0).getSurface());
+    	assertTrue(NamedEntityType.LOCANTPREFIX == nes.get(0).getType());
+    }
+    
+    @Test
+    public void testHandleTokenForTokenisedWordyPrefix() {
+    	NECollector collector = new NECollector();
+    	assertEquals(0, collector.getNes().size());
+    	
+    	Token t1 = new Token("exo", 0, 3, null, null, null);
+    	t1.setId(0);
+    	Token t2 = new Token("-", 3, 4, null, null, null);
+    	t2.setId(1);
+    	List <IToken> tokens = new ArrayList<IToken>();
+    	tokens.add(t1);
+    	tokens.add(t2);
+    	TokenSequence tokSeq = new TokenSequence("exo-", 0, null, tokens);
+    	t1.setTokenSequence(tokSeq);
+    	t2.setTokenSequence(tokSeq);
+    	
+    	DFAONTCPRFinder.getInstance().handleTokenForPrefix(t1, collector);
+    	assertEquals(0, collector.getNes().size());
+    	
+    	DFAONTCPRFinder.getInstance().handleTokenForPrefix(t2, collector);
+    	List <NamedEntity> nes = collector.getNes(); 
+    	assertEquals(1, nes.size());
+    	assertEquals("exo-", nes.get(0).getSurface());
+    	assertTrue(NamedEntityType.LOCANTPREFIX == nes.get(0).getType());
+    }
+}

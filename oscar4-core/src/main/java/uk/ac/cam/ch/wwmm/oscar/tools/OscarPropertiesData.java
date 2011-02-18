1658bd2e4fcb4716f10dc3283b2ea125e80ed5f8
dmj30 <dmj30@localhost>
2011-02-18 15:22:41 +0000

    Removed unused properties

--- oscar4-core/src/main/java/uk/ac/cam/ch/wwmm/oscar/tools/OscarPropertiesData.java
+++ oscar4-core/src/main/java/uk/ac/cam/ch/wwmm/oscar/tools/OscarPropertiesData.java
@@ -12,27 +12,14 @@ package uk.ac.cam.ch.wwmm.oscar.tools;
  */
 public class OscarPropertiesData {
 
-	public boolean fulldb;
-	public boolean lockdown;
-	public boolean makeCML;
 	public boolean splitOnEnDash;
 	public boolean useONT;
 	public boolean useDSO;
 	public boolean deprioritiseONT;
 	public boolean useFormulaRegex;
 	public boolean useWordShapeHeuristic;
-	public boolean minimizeDFA;
-	public boolean useJNIInChI;
-	public boolean useMEMM;
-	public boolean rescoreMEMM;
-	public boolean interpretPoly;
 	public boolean dataOnlyInExperimental;
 	public boolean polymerMode;
-	public boolean cacheExtensionNameResolver;
-	public boolean useOPSIN;
-	public boolean scrapBookIEFix;
-	public boolean urlEncodeCML;
-	public boolean useCachedResources; 
 		
 	public double ngramThreshold;
 	public double ontProb;
@@ -40,37 +27,7 @@ public class OscarPropertiesData {
 	public double custProb;
 	public double neThreshold;
 
-	public int dfaSize;
-	public int port;
-	
-	public String dbname;
-	public String dbaddress;
-	public String dbusername;
-	public String dbpasswd;
-	public String rdbms;
-	public String safdbusername;
-	public String safdbpasswd;
-	
-	public String serverType;
-	
-	public String hostname;
-	
-	public String oscarFlow;
-	
-	public String geniaPath;
-	public String pcdir;
-	public String InChI;
-	public String stdInChI;
-	public String svdlibc;
-	public String openBabel;
 	public String model;
-	public String yahooKey;
-	public String extensionNameResolver;
-	public String initScript;
-	public String resourcePrefix;
-	public String chemNameDict;
-	public String serverRoot;
 	public String xmlStrings;
-	public String chemicalEntityRecogniser;
 
 }

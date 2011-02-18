4b29b9dbe00ab3c5d1085da0f079ff018def1bb2
dmj30 <dmj30@localhost>
2011-02-18 15:22:41 +0000

    Removed unused properties

--- oscar4-core/src/main/java/uk/ac/cam/ch/wwmm/oscar/tools/OscarProperties.java
+++ oscar4-core/src/main/java/uk/ac/cam/ch/wwmm/oscar/tools/OscarProperties.java
@@ -53,28 +53,14 @@ public class OscarProperties {
 
 	private Properties variablesToProps() {
 		Properties props = new Properties();
-		props.setProperty("fulldb", oscarProperties.fulldb ? "yes" : "no");
-		props.setProperty("lockdown", oscarProperties.lockdown ? "yes" : "no");
-		props.setProperty("makeCML", oscarProperties.makeCML ? "yes" : "no");
 		props.setProperty("splitOnEnDash", oscarProperties.splitOnEnDash ? "yes" : "no");
 		props.setProperty("useONT", oscarProperties.useONT ? "yes" : "no");
 		props.setProperty("useDSO", oscarProperties.useDSO ? "yes" : "no");
 		props.setProperty("deprioritiseONT", oscarProperties.deprioritiseONT ? "yes" : "no");
 		props.setProperty("useFormulaRegex", oscarProperties.useFormulaRegex ? "yes" : "no");
 		props.setProperty("useWordShapeHeuristic", oscarProperties.useWordShapeHeuristic ? "yes" : "no");
-		props.setProperty("minimizeDFA", oscarProperties.minimizeDFA ? "yes" : "no");
-		props.setProperty("useJNIInChI", oscarProperties.useJNIInChI ? "yes" : "no");
-		props.setProperty("useMEMM", oscarProperties.useMEMM ? "yes" : "no");
-		props.setProperty("rescoreMEMM", oscarProperties.rescoreMEMM ? "yes" : "no");
-		props.setProperty("interpretPoly", oscarProperties.interpretPoly ? "yes" : "no");
 		props.setProperty("dataOnlyInExperimental", oscarProperties.dataOnlyInExperimental ? "yes" : "no");
-		props.setProperty("chemicalEntityRecogniser", oscarProperties.chemicalEntityRecogniser);
 		props.setProperty("polymerMode", oscarProperties.polymerMode ? "yes" : "no");
-		props.setProperty("cacheExtensionNameResolver", oscarProperties.cacheExtensionNameResolver ? "yes" : "no");
-		props.setProperty("useOPSIN", oscarProperties.useOPSIN ? "yes" : "no");
-		props.setProperty("scrapBookIEFix", oscarProperties.scrapBookIEFix ? "yes" : "no");
-		props.setProperty("urlEncodeCML", oscarProperties.urlEncodeCML ? "yes" : "no");
-		props.setProperty("useCachedResources", oscarProperties.useCachedResources ? "yes" : "no");
 		
 		props.setProperty("ngramThreshold", "" + oscarProperties.ngramThreshold);
 		props.setProperty("neThreshold", "" + oscarProperties.neThreshold);
@@ -82,63 +68,20 @@ public class OscarProperties {
 		props.setProperty("cprProb", "" + oscarProperties.cprProb);
 		props.setProperty("custProb", "" + oscarProperties.custProb);
 
-		props.setProperty("dfaSize", "" + oscarProperties.dfaSize);
-		props.setProperty("port", "" + oscarProperties.port);
-		
-		props.setProperty("chemicalEntityRecogniser", oscarProperties.chemicalEntityRecogniser);
-		props.setProperty("dbaddress", oscarProperties.dbaddress);
-		props.setProperty("dbusername", oscarProperties.dbusername);
-		props.setProperty("dbpasswd", oscarProperties.dbpasswd);
-		props.setProperty("rdbms", oscarProperties.rdbms);
-		props.setProperty("safdbusername", oscarProperties.safdbusername);
-		props.setProperty("safdbpasswd", oscarProperties.safdbpasswd);
-		props.setProperty("serverType", oscarProperties.serverType);
-		props.setProperty("hostname", oscarProperties.hostname);
-		props.setProperty("oscarFlow", oscarProperties.oscarFlow);
-		props.setProperty("geniaPath", oscarProperties.geniaPath);
-		props.setProperty("pcdir", oscarProperties.pcdir);
-		props.setProperty("InChI", oscarProperties.InChI);
-		props.setProperty("stdInChI", oscarProperties.stdInChI);
-		props.setProperty("openBabel", oscarProperties.openBabel);
-		props.setProperty("svdlibc", oscarProperties.svdlibc);
 		props.setProperty("model", oscarProperties.model);
-		props.setProperty("yahooKey", oscarProperties.yahooKey);
-		props.setProperty("extensionNameResolver", oscarProperties.extensionNameResolver);
-		props.setProperty("initScript", oscarProperties.initScript);
-		props.setProperty("resourcePrefix", oscarProperties.resourcePrefix);
-		props.setProperty("chemNameDict", oscarProperties.chemNameDict);
-		props.setProperty("serverRoot", oscarProperties.serverRoot);
 		props.setProperty("xmlStrings", oscarProperties.xmlStrings);
 		return props;
 	}
 
 	private void propsToVariables(Properties myProperties) {
-		oscarProperties.fulldb = "yes".equals(myProperties.getProperty("fulldb"));
-		oscarProperties.lockdown = "yes".equals(myProperties.getProperty("lockdown"));
-		oscarProperties.makeCML = "yes".equals(myProperties.getProperty("makeCML"));
 		oscarProperties.splitOnEnDash = "yes".equals(myProperties.getProperty("splitOnEnDash"));
 		oscarProperties.useONT = "yes".equals(myProperties.getProperty("useONT"));
 		oscarProperties.useDSO = "yes".equals(myProperties.getProperty("useDSO"));
 		oscarProperties.deprioritiseONT = "yes".equals(myProperties.getProperty("deprioritiseONT"));
 		oscarProperties.useFormulaRegex = "yes".equals(myProperties.getProperty("useFormulaRegex"));
 		oscarProperties.useWordShapeHeuristic = "yes".equals(myProperties.getProperty("useWordShapeHeuristic"));
-		oscarProperties.minimizeDFA = "yes".equals(myProperties.getProperty("minimizeDFA"));
-		oscarProperties.useJNIInChI = "yes".equals(myProperties.getProperty("useJNIInChI"));
-		oscarProperties.useMEMM = "yes".equals(myProperties.getProperty("useMEMM"));
-		oscarProperties.rescoreMEMM = "yes".equals(myProperties.getProperty("rescoreMEMM"));
-		oscarProperties.interpretPoly = "yes".equals(myProperties.getProperty("interpretPoly"));
 		oscarProperties.dataOnlyInExperimental = "yes".equals(myProperties.getProperty("dataOnlyInExperimental"));
-		oscarProperties.chemicalEntityRecogniser = getPropertyOrNone(myProperties, "chemicalEntityRecogniser");
 		oscarProperties.polymerMode = "yes".equals(myProperties.getProperty("polymerMode"));
-		//override useMEMM if operating in polymerMode
-		if (oscarProperties.polymerMode) {
-			oscarProperties.useMEMM = false;
-		}
-		oscarProperties.cacheExtensionNameResolver = "yes".equals(myProperties.getProperty("cacheExtensionNameResolver"));
-		oscarProperties.useOPSIN = "yes".equals(myProperties.getProperty("useOPSIN"));
-		oscarProperties.scrapBookIEFix = "yes".equals(myProperties.getProperty("scrapBookIEFix"));
-		oscarProperties.urlEncodeCML = "yes".equals(myProperties.getProperty("urlEncodeCML"));
-		oscarProperties.useCachedResources = "yes".equals(myProperties.getProperty("useCachedResources"));
 		
 		oscarProperties.ngramThreshold = Double.parseDouble(myProperties.getProperty("ngramThreshold"));
 		oscarProperties.neThreshold = Double.parseDouble(myProperties.getProperty("neThreshold"));
@@ -146,32 +89,7 @@ public class OscarProperties {
 		oscarProperties.cprProb = Double.parseDouble(myProperties.getProperty("cprProb"));
 		oscarProperties.custProb = Double.parseDouble(myProperties.getProperty("custProb"));
 
-		oscarProperties.dfaSize = Integer.parseInt(myProperties.getProperty("dfaSize"));
-		oscarProperties.port = Integer.parseInt(myProperties.getProperty("port"));
-		
-		oscarProperties.dbname = getPropertyOrNone(myProperties, "dbname");
-		oscarProperties.dbaddress = getPropertyOrNone(myProperties, "dbaddress");
-		oscarProperties.dbusername = getPropertyOrNone(myProperties, "dbusername");
-		oscarProperties.dbpasswd = getPropertyOrNone(myProperties, "dbpasswd");
-		oscarProperties.rdbms = getPropertyOrNone(myProperties, "rdbms");
-		oscarProperties.safdbusername = getPropertyOrNone(myProperties, "safdbusername");
-		oscarProperties.safdbpasswd = getPropertyOrNone(myProperties, "safdbpasswd");
-		oscarProperties.serverType = getPropertyOrNone(myProperties, "serverType");
-		oscarProperties.hostname = getPropertyOrNone(myProperties, "hostname");
-		oscarProperties.oscarFlow = getPropertyOrNone(myProperties, "oscarFlow");
-		oscarProperties.geniaPath = getPropertyOrNone(myProperties, "geniaPath");
-		oscarProperties.pcdir = getPropertyOrNone(myProperties, "pcdir");
-		oscarProperties.InChI = getPropertyOrNone(myProperties, "InChI");
-		oscarProperties.stdInChI = getPropertyOrNone(myProperties, "stdInChI");
-		oscarProperties.openBabel = getPropertyOrNone(myProperties, "openBabel");
-		oscarProperties.svdlibc = getPropertyOrNone(myProperties, "svdlibc");
 		oscarProperties.model = getPropertyOrNone(myProperties, "model");
-		oscarProperties.yahooKey = getPropertyOrNone(myProperties, "yahooKey");
-		oscarProperties.extensionNameResolver = getPropertyOrNone(myProperties, "extensionNameResolver");
-		oscarProperties.initScript = getPropertyOrNone(myProperties, "initScript");
-		oscarProperties.resourcePrefix = getPropertyOrNone(myProperties, "resourcePrefix");
-		oscarProperties.chemNameDict = getPropertyOrNone(myProperties, "chemNameDict");
-		oscarProperties.serverRoot = getPropertyOrNone(myProperties, "serverRoot");
 		oscarProperties.xmlStrings = getPropertyOrNone(myProperties, "xmlStrings");
 	}
 

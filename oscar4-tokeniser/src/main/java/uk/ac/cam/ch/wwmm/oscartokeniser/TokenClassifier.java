05ca650067fac1b9f4b914e2065e80e078425151
lh359 <lh359@cam.ac.uk>
2011-03-14 12:33:12 +0000

    removed the uima descriptor files from oscar4-tokeniser

--- oscar4-tokeniser/src/main/java/uk/ac/cam/ch/wwmm/oscartokeniser/TokenClassifier.java
+++ oscar4-tokeniser/src/main/java/uk/ac/cam/ch/wwmm/oscartokeniser/TokenClassifier.java
@@ -13,8 +13,6 @@ import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import nu.xom.Document;
 import nu.xom.Element;

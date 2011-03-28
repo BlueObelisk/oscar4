05ade54d69b617445ba8b59ab33d7ca771a624e1
dmj30 <dmj30@cam.ac.uk>
2011-03-28 19:07:12 +0100

    Removed currentInstance from ManualAnnotations.

--- oscar4-recogniser-core/src/test/java/uk/ac/cam/ch/wwmm/oscarrecogniser/manualAnnotations/ManualAnnotationsTest.java
+++ oscar4-recogniser-core/src/test/java/uk/ac/cam/ch/wwmm/oscarrecogniser/manualAnnotations/ManualAnnotationsTest.java
@@ -1,16 +1,8 @@
 package uk.ac.cam.ch.wwmm.oscarrecogniser.manualAnnotations;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
+import static org.junit.Assert.*;
 import nu.xom.Document;
 import nu.xom.Element;
-import nu.xom.ParsingException;
-import nu.xom.ValidityException;
 
 import org.junit.Test;
 
@@ -36,25 +28,14 @@ public class ManualAnnotationsTest {
 		assertTrue(manualAnnotations.getChemicalWords().contains("ammonia"));
 	}
 
-//  XXX Becoming immutable
-//	@Test
-//	public void testClear() {
-//		ExtractedTrainingData etd = ExtractedTrainingData.getInstance();
-//		assertFalse(etd.chemicalWords.size() == 0);
-//
-//		etd.clear();
-//		ExtractedTrainingData etd2 = ExtractedTrainingData.getInstance();
-//		assertTrue(etd == etd2);
-//		assertTrue(etd.chemicalWords.size() == 0);
-//	}
 	
 	@Test
 	public void testReinitialise() {
-		ManualAnnotations annotations1 = ManualAnnotations.reinitialise(ManualAnnotations.loadEtdElement("chempapers"));
+		ManualAnnotations annotations1 = new ManualAnnotations(ManualAnnotations.loadEtdElement("chempapers"));
 		assertTrue(annotations1.getNonChemicalWords().contains("elongate"));
 		assertFalse(annotations1.getNonChemicalWords().contains("leukaemic"));
 		
-		ManualAnnotations annotations2 = ManualAnnotations.reinitialise(ManualAnnotations.loadEtdElement("pubmed"));
+		ManualAnnotations annotations2 = new ManualAnnotations(ManualAnnotations.loadEtdElement("pubmed"));
 		assertFalse(annotations2.getNonChemicalWords().contains("elongate"));
 		assertTrue(annotations2.getNonChemicalWords().contains("leukaemic"));
 	}

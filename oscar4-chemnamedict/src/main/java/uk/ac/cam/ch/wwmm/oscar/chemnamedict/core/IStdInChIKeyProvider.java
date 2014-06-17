f25f3165e6976cb85d14b4db0acadbe96baa6fbc
Mark J. Williamson <mjw@mjw.name>
2014-06-17 09:35:56 +0100

    Javadoc updates

--- oscar4-chemnamedict/src/main/java/uk/ac/cam/ch/wwmm/oscar/chemnamedict/core/IStdInChIKeyProvider.java
+++ oscar4-chemnamedict/src/main/java/uk/ac/cam/ch/wwmm/oscar/chemnamedict/core/IStdInChIKeyProvider.java
@@ -4,14 +4,14 @@ import java.util.Set;
 
 /**
  * Extension of the {@link IChemNameDict} interface to return hits
- * in InChI.
+ * in Standard InChI Key.
  *
  * @author mjw
  */
 public interface IStdInChIKeyProvider {
 
 	/**
-	 * Returns a set containing all of the known InChIs for
+	 * Returns a set containing all of the known Standard InChI Keys for
 	 * the given query name.
 	 */
 	public Set<String> getStdInchiKeys(String queryName);
